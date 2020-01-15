4c1c5f124abfdba1d3d94393d50545002105da5c
rocketpy <dimitr.ushakov@gmail.com>
2020-01-15 21:49:03 +0200

    modified

--- API_example.py
+++ API_example.py
@@ -1,1 +1,10 @@
+import requests
 
+
+url = "https://api.github.com"
+
+response = requests.get(url)
+print("Status code is : ", response.status_code)
+
+response_json = response.json()
+print(response_json)
