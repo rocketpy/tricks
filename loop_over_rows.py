431ce80f1da387e51523190eb9d897b9edb31705
rocketpy <dimitr.ushakov@gmail.com>
2020-02-05 23:31:49 +0200

    modified

--- loop_over_rows.py
+++ loop_over_rows.py
@@ -1,1 +1,9 @@
+import csv
 
+#  we can use this for sending emails
+with open("file.csv") as file:
+    reader = csv.reader(file)
+    next(reader)  # skip header row
+    for name, email, grade in reader:
+        print(f"Sending email to {name}")
+        # send email here
