e9f38d2f7c280e3d88c6a7c07b43d57d067a2ad2
rocketpy <dimitr.ushakov@gmail.com>
2020-06-26 17:46:31 +0300

    updated

--- progress_bar.py
+++ progress_bar.py
@@ -9,3 +9,18 @@ some_text = ""
 for elem in tqdm(["a", "b", "c", "d"]):  # need wrap tqdm() around any iterable
     sleep(0.5)
     some_text = some_text + elem
+
+# or use progress.bar
+from time import sleep
+from progress.bar import IncrementalBar
+
+
+some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+bar = IncrementalBar('Left : ', max=len(some_list))
+
+for elem in some_list:
+    bar.next()
+    time.sleep(0.5)
+    
+bar.finish()
+
