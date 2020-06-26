3d8e04a75367c1a33cda7652d8dce85812d397fb
rocketpy <dimitr.ushakov@gmail.com>
2020-06-26 17:36:06 +0300

    updated

--- progress_bar.py
+++ progress_bar.py
@@ -1,1 +1,11 @@
+# https://pypi.org/project/tqdm/
+# pip install tqdm
 
+from tqdm import tqdm
+from time import sleep
+
+
+some_text = ""
+for elem in tqdm(["a", "b", "c", "d"]):  # need wrap tqdm() around any iterable
+    sleep(0.5)
+    some_text = some_text + elem
