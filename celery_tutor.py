140673956dac7103a37c4d72ce421907502ec3b9
rocketpy <dimitr.ushakov@gmail.com>
2020-07-05 23:43:13 +0300

    updated

--- celery_tutor.py
+++ celery_tutor.py
@@ -1,1 +1,15 @@
+# Celery is  Distributed Task Queue !
+# pip install celery
 
+# https://docs.celeryproject.org/en/latest/getting-started/first-steps-with-celery.html  - First Steps with Celery
+# https://docs.celeryproject.org/en/latest/getting-started/next-steps.html  -  Next Steps
+
+#  Simple example ( ... taked from https://pypi.org/project/celery/ )
+from celery import Celery
+
+app = Celery('hello', broker='amqp://guest@localhost//')
+
+@app.task
+def hello():
+    return 'hello world'
+  
