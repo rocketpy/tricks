96befc771dfeb15f2e3c3ecdd73f1cbc8e68025c
rocketpy <dimitr.ushakov@gmail.com>
2020-07-17 23:58:50 +0300

    updated

--- pymorphy.py
+++ pymorphy.py
@@ -2,9 +2,22 @@
 # https://github.com/kmike/pymorphy2
 # pip install pymorphy
 
+#  simple example , open some file with text and use pymorphy2
 import re
 import pymorphy2
 
 
 morph = pymorphy2.MorphAnalyzer()
 
+with open('file_name.txt',  encoding='utf-8') as f:
+    words = [
+        [morph.parse(word)[0].normal_form for word in line.re.findall(r'\w+')]  #  split()
+        for line in f
+    ]
+
+print(words)
+
+"""
+str = 'If this is not possible, for example on a remote CI server !'
+print(re.findall(r'\w+', str)) 
+"""
