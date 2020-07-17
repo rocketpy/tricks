e1e1aec796a7caa8aee570c9f7fabfa128e66374
rocketpy <dimitr.ushakov@gmail.com>
2020-07-18 00:16:16 +0300

    updated

--- clean_text.py
+++ clean_text.py
@@ -1,1 +1,9 @@
+# remove some symbols from text 
 
+forbidden = ('.','?','!',':',';','-','—',' ',)
+
+some_string = somestring.lower()  # text to lowercase
+
+result = "".join(i for i in somes_tring if i not in forbidden)  # removed all symbols 
+
+res = "".join(i for i in some_string if symbol.isalnum())  # result will contain only letters and numbers
