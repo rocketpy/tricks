37a02e8f72f6a37f980d0a23d70d2aec49f13cd8
rocketpy <dimitr.ushakov@gmail.com>
2020-07-22 23:38:48 +0300

    updated

--- reading_and_storing_img.py
+++ reading_and_storing_img.py
@@ -1,1 +1,12 @@
+from PIL import Image
+import csv
+#  reading from disk 
+
+#  read a single image and its meta from a .png and .csv file
+def read_single_disk(image_id):
+    img = np.array(Image.open(disk_dir / f"{image_id}.png"))
+    with open(disk_dir / f"{image_id}.csv", "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        label = int(next(reader)[0])
+    return img, label
 
