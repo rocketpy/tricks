214e77b2f524a7fcf2dfcffad44a15f827147f0b
rocketpy <dimitr.ushakov@gmail.com>
2020-08-07 23:52:43 +0300

    updated

--- image_by_webcam.py
+++ image_by_webcam.py
@@ -1,1 +1,14 @@
+import cv2
 
+
+# by first cam
+img = cv2.VideoCapture(0)
+ret, frame = img.read()
+cv2.imwrite('file_name.jpg', frame)  # or .png
+img.release()
+
+# by second cam , if two cams
+img_sec = cv2.VideoCapture(1)
+ret, frame = img_sec.read()
+cv2.imwrite('file_name.jpg', frame)  # or .png
+img_sec.release()
