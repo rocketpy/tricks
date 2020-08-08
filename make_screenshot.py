90994f4d28d323468e9e3ba19fe94deeb42ee78a
rocketpy <dimitr.ushakov@gmail.com>
2020-08-08 23:26:22 +0300

    updated

--- make_screenshot.py
+++ make_screenshot.py
@@ -1,1 +1,10 @@
+#  docs: https://pypi.org/project/PyAutoGUI/
+#  pip install pyautogui
 
+import pyautogui
+
+
+img = pyautogui.screenshot()
+img.save('my_img.png')
+# or
+img_2 = pyautogui.screenshot('my_img_2.png')
