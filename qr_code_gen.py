75fb32ffec5bd50d44a4b3637d3eda2d3fca5cfc
rocketpy <dimitr.ushakov@gmail.com>
2020-09-12 23:36:50 +0300

    added a main code

--- qr_code_gen.py
+++ qr_code_gen.py
@@ -1,1 +1,25 @@
+#  docs:  https://github.com/lincolnloop/python-qrcode
+#  pip:  https://pypi.org/project/qrcode/
 
+#  pip install qrcode
+
+
+# base examples 
+import qrcode
+
+img = qrcode.make('Some data here')
+
+#  or
+
+import qrcode
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
