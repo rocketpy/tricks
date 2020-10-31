dc0f854481669bb17ae635012930f7350c236e47
rocketpy <dimitr.ushakov@gmail.com>
2020-10-31 15:50:33 +0200

    updated

--- create_fake_data_Faker.py
+++ create_fake_data_Faker.py
@@ -1,6 +1,21 @@
-# https://faker.readthedocs.io/en/latest/index.html
+#  Docs:  https://faker.readthedocs.io/en/latest/index.html
+#  Using the Faker Class :  https://faker.readthedocs.io/en/latest/fakerclass.html
 
 #  pip install Faker
 
 from faker import Faker
+
+
 fake = Faker()
+
+fake.name()
+# 'Lucy Cechtelar'
+
+fake.address()
+# '426 Jordy Lodge
+#  Cartwrightshire, SC 88120-6700'
+
+fake.text()
+#  Aut molestias et maxime. Fugit autem facilis quos vero. Eius quibusdam possimus est.
+#  Ea quaerat et quisquam. Deleniti sunt quam. Adipisci consequatur id in occaecati.
+#  Et sint et. Ut ducimus quod nemo ab voluptatum.'
