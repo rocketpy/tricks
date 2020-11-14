ca11a9dba356fa81bd40984036e491665847cc68
rocketpy <dimitr.ushakov@gmail.com>
2020-11-14 19:19:05 +0200

    updated

--- recognizing_text_from_picture.py
+++ recognizing_text_from_picture.py
@@ -1,1 +1,8 @@
+import pytesseract
+from PIL import Image
+
+
+img = Image.open("file_name.png")  #  .jpg
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
 
