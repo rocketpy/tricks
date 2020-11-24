ebd8134f551f85bea754a159195fc9e95cda5b8a
rocketpy <dimitr.ushakov@gmail.com>
2020-11-24 18:09:48 +0200

    updated

--- make_pings.py
+++ make_pings.py
@@ -1,1 +1,9 @@
+#  pip install pythonping
+#  DOCS:  https://pypi.org/project/pythonping/  or  https://github.com/alessandromaggio/pythonping
 
+#  PythonPing is simple way to ping in Python.
+
+from pythonping import ping
+
+
+ping('127.0.0.1', verbose=True)
