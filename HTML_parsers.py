f95098eb0a67ab7e7622a33c753a7f1257669972
rocketpy <dimitr.ushakov@gmail.com>
2020-12-05 17:26:57 +0200

    updated

--- HTML_parsers.py
+++ HTML_parsers.py
@@ -5,3 +5,13 @@ from bs4 import BeautifulSoup
 url = 'https://www...'
 html_text = requests.get(url).text
 soup = BeautifulSoup(html_text, 'html.parser')
+
+
+# or using urllib
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+
+page = urllib2.urlopen('http://www...')
+soup = BeautifulSoup(page)
+x = soup.body.find('div', attrs={'class' : 'container'}).text
