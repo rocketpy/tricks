e90f99a21f0f932d949d5c9d37b2b9d66dde6ddd
rocketpy <dimitr.ushakov@gmail.com>
2020-12-09 15:34:17 +0200

    updated

--- svg_graph_plotting.py
+++ svg_graph_plotting.py
@@ -1,1 +1,7 @@
+# Pygal is a python svg graph plotting library
 
+# https://pypi.org/project/pygal/
+
+# pip install pygal
+
+# IMPORTANT: for check a result need use render_to_file() , and open file in browser for checking a result !!!
