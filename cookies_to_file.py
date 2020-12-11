d72aa4d152f1fdf1b589e5e529f4af6d66dd0c34
rocketpy <dimitr.ushakov@gmail.com>
2020-12-11 22:34:39 +0200

    updated

--- cookies_to_file.py
+++ cookies_to_file.py
@@ -1,1 +1,11 @@
+#  Using a pickle, to save cookies as text file and load it later !
 
+def save_cookie(driver, path):
+    with open(path, 'wb') as filehandler:
+        pickle.dump(driver.get_cookies(), filehandler)
+
+def load_cookie(driver, path):
+     with open(path, 'rb') as cookiesfile:
+         cookies = pickle.load(cookiesfile)
+         for cookie in cookies:
+             driver.add_cookie(cookie)
