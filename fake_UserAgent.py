6d9f36ed6fca35a7a092f4c890e760edf17070ae
rocketpy <dimitr.ushakov@gmail.com>
2020-12-15 15:03:24 +0200

    updated

--- fake_UserAgent.py
+++ fake_UserAgent.py
@@ -1,3 +1,5 @@
+#  PyPi:  https://pypi.org/project/fake-useragent/
+
 from selenium import webdriver
 from fake_useragent import UserAgent
 from selenium.webdriver.chrome.options import Options
@@ -5,10 +7,16 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 ua = UserAgent()
-userAgent = ua.random
+userAgent = ua.random  #  best one, random via real world browser usage statistic
 # print(userAgent)
 options.add_argument(f'user-agent={userAgent}')
 driver = webdriver.Chrome(chrome_options=options, executable_path=r'C:\\chromedriver.exe')
 driver.get("https://www...")
 
 driver.quit()
+
+
+#  If You don’t want cache database or no writable file system:
+from fake_useragent import UserAgent
+
+ua = UserAgent(cache=False)
