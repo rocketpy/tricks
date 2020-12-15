125aabb87726961ec9b41ae4be741bf6aebc4407
rocketpy <dimitr.ushakov@gmail.com>
2020-12-15 14:59:51 +0200

    updated

--- fake_UserAgent.py
+++ fake_UserAgent.py
@@ -1,1 +1,14 @@
+from selenium import webdriver
+from fake_useragent import UserAgent
+from selenium.webdriver.chrome.options import Options
 
+
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+# print(userAgent)
+options.add_argument(f'user-agent={userAgent}')
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'C:\\chromedriver.exe')
+driver.get("https://www...")
+
+driver.quit()
