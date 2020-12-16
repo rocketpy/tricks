48120b046865ce26b6e089712ba4abd439a3fcf0
rocketpy <dimitr.ushakov@gmail.com>
2020-12-16 20:09:00 +0200

    updated

--- CMD_commands.py
+++ CMD_commands.py
@@ -1,4 +1,23 @@
-#  pip install virtualenv
-#  virtualenv env
-#  env/bin/activate
-#  git clone https://github.com/...git
+#  pip install virtualenv  -  installing venv
+
+#  python -m venv name_venv  -  creating venv Windows
+
+#  myvenv\Scripts\activate  -  activating venv Windows
+
+#  myvenv\deactivate  - deactivating venv Windows
+
+#  git clone https://github.com/...git  -  clone github repo
+
+#  python -m pip install --upgrade pip  
+
+#  pip install -r requirements.txt
+
+#  pip freeze  - show list requirements.txt
+
+#  pip freeze > requirements.txt  -  save installed modules to file
+
+#  pip install flake8
+
+#  pip install django == 2.1.1  -  install some version
+
+#  pip install --upgrade module_name  -  update some module
