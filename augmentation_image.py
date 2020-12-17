c34339d848d3609c98185d40f541205b8c3e6ee4
rocketpy <dimitr.ushakov@gmail.com>
2020-12-17 21:44:38 +0200

    updated

--- augmentation_image.py
+++ augmentation_image.py
@@ -4,3 +4,21 @@
 #  Github:  https://github.com/albumentations-team/albumentations
 
 #  pip install -U albumentations
+
+#  simple example
+import cv2
+import albumentations as A
+
+
+transform = A.Compose([A.RandomCrop(width=256, height=256),
+                       A.HorizontalFlip(p=0.5),
+                       A.RandomBrightnessContrast(p=0.2),
+                      ])
+
+# Read an image with OpenCV and convert it to the RGB colorspace
+image = cv2.imread("image.jpg")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# Augment an image
+transformed = transform(image=image)
+transformed_image = transformed["image"]
