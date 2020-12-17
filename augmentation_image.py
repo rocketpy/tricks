d8138d2afd56c27a9da6c9113b67d5c7a67d42e2
rocketpy <dimitr.ushakov@gmail.com>
2020-12-17 21:42:23 +0200

    updated

--- augmentation_image.py
+++ augmentation_image.py
@@ -1,1 +1,6 @@
+#  Albumentations is a fast and flexible image augmentation library.
 
+#  Docs:  https://albumentations.ai/docs/
+#  Github:  https://github.com/albumentations-team/albumentations
+
+#  pip install -U albumentations
