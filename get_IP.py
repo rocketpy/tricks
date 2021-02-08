66050c8196ac72f90df8aab17dde6fe04cac59c6
rocketpy <dimitr.ushakov@gmail.com>
2021-02-08 22:35:25 +0200

    Update get_IP.py

--- get_IP.py
+++ get_IP.py
@@ -1,1 +1,13 @@
+import re
+import requests
 
+
+try:
+    result = requests.get("https://www.whatismyip.org/")
+    # IP by CSS-selector:  #collapse-menu > h3 > a
+    # IP by xPath:  //*[@id="collapse-menu"]/h3/a
+    IP = re.compile('(\d{1,3}\.){3}\d{1,3}').search(result.text).group()
+    if IP != "":
+        print(IP)
+except:
+    print('Some error !')
