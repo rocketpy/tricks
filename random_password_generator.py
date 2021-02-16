d71adcf81ae0115bd2397d40b50f99f96e4f24e2
rocketpy <dimitr.ushakov@gmail.com>
2021-02-16 21:21:48 +0200

    Update random_password_generator.py

--- random_password_generator.py
+++ random_password_generator.py
@@ -26,4 +26,14 @@ def password_generator():
     return ''.join(result)
 """
     
+"""
+import string
+import random
+
+
+def password_generator():
+    chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    size = random.randint(8, 12)
+    return ''.join(random.choice(chars) for x in range(size))
+"""
     
