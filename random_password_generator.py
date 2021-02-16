2781a2f79ce919ca9b9a69ecab04b5e97637eccf
rocketpy <dimitr.ushakov@gmail.com>
2021-02-16 21:12:10 +0200

    Update random_password_generator.py

--- random_password_generator.py
+++ random_password_generator.py
@@ -5,12 +5,10 @@ import random
 #  size: default=8
 #  chars: default=A-Za-z0-9 
 
-
 def password_generator(size=8, chars=string.ascii_letters + string.digits):
     return ''.join(random.choice(chars) for i in range(size))
  
 
-    
 """
 import string
 import random
@@ -23,7 +21,9 @@ def password_generator():
     other_characters = [random.choice(string.ascii_letters + string.digits)
                        for index in range(random.randint(3, 17))
                        ]
-
+    result = [lower_case_letter, upper_case_letter] + other_characters
+    random.shuffle(result)
+    return ''.join(result)
 """
     
     
