d4973a1bda39a0c5d0d31a9ebd72ae8863714b44
rocketpy <dimitr.ushakov@gmail.com>
2021-02-17 21:33:00 +0200

    Update PyUSB.py

--- PyUSB.py
+++ PyUSB.py
@@ -1,1 +1,11 @@
+#  Python USB access module
 
+# PyPi:https://pypi.org/project/pyusb/
+# Github - Tutorial: https://github.com/pyusb/pyusb/blob/master/docs/tutorial.rst
+
+# pip install pyusb
+
+import usb.core
+
+
+dev = usb.core.find()
