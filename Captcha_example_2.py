e2bc1552af455125c57b1db2e4778efd7f2e54be
rocketpy <dimitr.ushakov@gmail.com>
2021-02-20 21:01:21 +0200

    Update Captcha_example_2.py

--- Captcha_example_2.py
+++ Captcha_example_2.py
@@ -1,1 +1,14 @@
+import io                                                                       
+import urllib2  
+import scrapy                                                              
+import pytesseract     
+from PIL import Image  
+     
 
+class CaptchaSpider(scrapy.Spider):
+    name = 'captcha'                                                            
+
+    def start_requests(self):                                                   
+        yield scrapy.Request('https://...', cookies={'PHPSESSID': 'xyz'})                      
+
+  
