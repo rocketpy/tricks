369647a54ad6f984ed0c2204036f8c6b3fc6b4a7
rocketpy <dimitr.ushakov@gmail.com>
2021-02-20 21:17:50 +0200

    Update Captcha_example.py

--- Captcha_example.py
+++ Captcha_example.py
@@ -1,15 +1,26 @@
 import cv2
 import pytesseract
 from PIL import Image
-from pdf2image import convert_from_path
+from pdf2image import convert_from_path, convert_from_bytes
 
 
-#img = Image.open('captcha.jpg')
+# imgage = Image.open('captcha.jpg')
+
 image = cv2.imread('captcha.jpg')
+
+"""
+path = ("https://captcha_url")
+file = open('captcha.jpg', 'wb')
+file.write(urllib.urlopen(path).read())
+file.close()
+"""
+
+# image = convert_from_path('/home/belval/example.pdf')
+# image = convert_from_bytes(open('/home/belval/example.pdf', 'rb').read())
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-#gray = cv2.medianBlur(gray, 3)
+# gray = cv2.medianBlur(gray, 3)
 
 filename = "{}.png".format("file_name")
 cv2.imwrite(filename, gray)
