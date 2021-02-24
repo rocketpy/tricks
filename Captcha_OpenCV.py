b82a4a21e738bffe907fc29991588955dbe18f4d
rocketpy <dimitr.ushakov@gmail.com>
2021-02-24 22:11:13 +0200

    Update Captcha_OpenCV.py

--- Captcha_OpenCV.py
+++ Captcha_OpenCV.py
@@ -1,1 +1,12 @@
+import cv2 as cv
+import numpy as np
 
+
+def deskew(img):
+    m = cv.moments(img)
+    if abs(m['mu02']) < 1e-2:
+        return img.copy()
+    skew = m['mu11']/m['mu02']
+    M = np.float32([[1, skew, -0.5*SZ*skew], [0, 1, 0]])
+    img = cv.warpAffine(img,M,(SZ, SZ),flags=affine_flags)
+    return img
