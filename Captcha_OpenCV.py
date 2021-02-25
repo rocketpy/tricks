90b75d6ccddf185573a9056151af4d0bb27dcde2
rocketpy <dimitr.ushakov@gmail.com>
2021-02-25 22:56:18 +0200

    Update Captcha_OpenCV.py

--- Captcha_OpenCV.py
+++ Captcha_OpenCV.py
@@ -29,4 +29,15 @@ def deskew(img):
     M = np.float32([[1, skew, -0.5*SZ*skew], [0, 1, 0]])
     img = cv.warpAffine(img,M,(SZ, SZ),flags=affine_flags)
     return img
+    
+def hog(img):
+    gx = cv.Sobel(img, cv.CV_32F, 1, 0)
+    gy = cv.Sobel(img, cv.CV_32F, 0, 1)
+    mag, ang = cv.cartToPolar(gx, gy)
+    bins = np.int32(bin_n*ang/(2*np.pi))    # quantizing binvalues in (0...16)
+    bin_cells = bins[:10,:10], bins[10:,:10], bins[:10,10:], bins[10:,10:]
+    mag_cells = mag[:10,:10], mag[10:,:10], mag[:10,10:], mag[10:,10:]
+    hists = [np.bincount(b.ravel(), m.ravel(), bin_n) for b, m in zip(bin_cells, mag_cells)]
+    hist = np.hstack(hists)     # hist is a 64 bit vector
+    return hist    
 """
