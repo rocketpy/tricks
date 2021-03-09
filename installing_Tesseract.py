ceab8b1de05ce20f808a8b889bd09c38a21b434e
rocketpy <dimitr.ushakov@gmail.com>
2021-03-09 23:49:44 +0200

    Update installing_Tesseract.py

--- installing_Tesseract.py
+++ installing_Tesseract.py
@@ -1,1 +1,13 @@
+#  Installing and using Tesseract 4 on windows 10
 
+#  Main Link for downloads
+#  https://digi.bib.uni-mannheim.de/tesseract/
+
+#  Very Important !!!
+"""
+Tick the boxes that say “ScrollView”, “Training Tools”, “Shortcuts creation” and importantly “Language data”.
+These should be ticked by default but just do them just in case they haven’t been ticked in your system.
+"""
+
+#  Directory 
+# C:\Program Files\Tesseract-OCR
