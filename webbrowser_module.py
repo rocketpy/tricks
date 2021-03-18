61da616ad97b8d6a2f0f8a85110701c2f081b6a2
rocketpy <dimitr.ushakov@gmail.com>
2021-03-18 23:36:47 +0200

    Update webbrowser_module.py

--- webbrowser_module.py
+++ webbrowser_module.py
@@ -1,1 +1,11 @@
+#  https://docs.python.org/3.6/library/webbrowser.html
 
+import webbrowser
+
+
+webbrowser.open('https://docs.python.org/3/library/webbrowser.html')
+
+webbrowser.open_new('https://docs.python.org/3/library/webbrowser.html')
+
+b = webbrowser.get('lynx')
+b.open('https://docs.python.org/3/library/webbrowser.html')
