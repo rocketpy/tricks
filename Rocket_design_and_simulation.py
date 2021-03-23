4a8eeca6b04f6e0a5eddd0d64b016b877c1e2118
rocketpy <dimitr.ushakov@gmail.com>
2021-03-23 18:06:46 +0200

    Update Rocket_design_and_simulation.py

--- Rocket_design_and_simulation.py
+++ Rocket_design_and_simulation.py
@@ -1,1 +1,10 @@
+#  rocketPy - Rocket design, simulation and analysis using Python !
+
+#  Docs: https://rocketPy.readthedocs.io
+#  Giyhub: https://github.com/dev10110/rocketPy
+
+# To use rocketPy in a project:
+
+import rocketPy
+
 
