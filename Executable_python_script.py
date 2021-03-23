6c2ae0b67d2cfd8a0242c672252375e83c166dbe
rocketpy <dimitr.ushakov@gmail.com>
2021-03-23 16:32:44 +0200

    Update Executable_python_script.py

--- Executable_python_script.py
+++ Executable_python_script.py
@@ -3,5 +3,12 @@
 # PyPi: https://pypi.org/project/cx-Freeze/
 # pip install cx-Freeze
 
+# In a virtual environment, install by issuing the command:
+# pip install cx_Freeze --upgrade
 
 
+# script file_name.py which you want to turn into an executable, by this command:
+#  cxfreeze -c hello.py --target-dir dist
+
+# Docs: to cxfreeze script
+# https://cx-freeze.readthedocs.io/en/latest/script.html#script
