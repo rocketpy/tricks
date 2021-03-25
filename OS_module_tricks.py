96058bc70ff9586eb1fa0e000213a1fa57ba37ca
rocketpy <dimitr.ushakov@gmail.com>
2021-03-25 23:13:49 +0200

    Update OS_module_tricks.py

--- OS_module_tricks.py
+++ OS_module_tricks.py
@@ -1,1 +1,17 @@
+import os
+
+
+#  Show current directory
+os.getcwd()
+
+#  Check if a file or directory exists
+os.path.exists('sample_data')
+
+#  Combining path components
+os.path.exists(os.path.join('sample_data', 'README.md'))
+
+#  Create directory
+os.mkdir('test_dir')
+
+
 
