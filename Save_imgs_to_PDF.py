e45a1a2078c7331201f87b135d10e2761b818f65
rocketpy <dimitr.ushakov@gmail.com>
2021-03-28 20:32:47 +0300

    Update Save_imgs_to_PDF.py

--- Save_imgs_to_PDF.py
+++ Save_imgs_to_PDF.py
@@ -1,1 +1,16 @@
+import os
+import img2pdf
 
+
+def imgs_to_pdf():
+    # print(os.listdir("Images"))  # print img names from Images dir
+    imgs_list = sorted(os.listdir("Images"))
+    
+    # creating PDF file        
+    with open("File with images.pdf", "wb") as f:
+        f.write(img2pdf.convert(imgs_list))
+    
+    
+if __name__ == '__main__':
+    imgs_to_pdf()
+    
