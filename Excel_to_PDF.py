c0161cef022a52a10cb60843dd160ab06f9b8701
rocketpy <dimitr.ushakov@gmail.com>
2021-04-05 22:33:13 +0300

    Update Excel_to_PDF.py

--- Excel_to_PDF.py
+++ Excel_to_PDF.py
@@ -1,6 +1,15 @@
 #  pip install pywin32
+#  pip install -U pypiwin32
 
 from win32com import client
   
 # Open Microsoft Excel
 excel = client.Dispatch("Excel.Application")
+
+# Read Excel File
+sheets = excel.Workbooks.Open('Excel File Path')
+work_sheets = sheets.Worksheets[0]
+  
+# Convert into PDF File
+work_sheets.ExportAsFixedFormat(0, 'PDF File Path')
+
