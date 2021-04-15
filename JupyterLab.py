5c3d1fb772a50246171acaf402d39c65cbac98b4
rocketpy <dimitr.ushakov@gmail.com>
2021-04-15 22:59:46 +0300

    Update JupyterLab.py

--- JupyterLab.py
+++ JupyterLab.py
@@ -1,1 +1,11 @@
+#  The JupyterLab server extension.
 
+#  Docs:  https://jupyterlab.readthedocs.io/en/stable/
+#  PyPi:  https://pypi.org/project/jupyterlab/
+#  Github:  https://github.com/jupyterlab/jupyterlab
+
+# pip install jupyterlab
+
+# Start JupyterLab using:  jupyter lab
+
+#  following browsers are currently known to work:  Firefox, Chrome
