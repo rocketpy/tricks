08ee05562f2fbc2e636b264bccd7e1faa427281c
rocketpy <dimitr.ushakov@gmail.com>
2021-04-19 21:52:56 +0300

    Update Image_pillow_tutor.py

--- Image_pillow_tutor.py
+++ Image_pillow_tutor.py
@@ -13,5 +13,23 @@ y //= 10
 smaller = pic.resize((x, y))
 print(smaller)
 
+#  border detection
+from PIL import ImageFilter
 
+edges = smaller.filter(ImageFilter.FIND_EDGES)
+print(edges)
+
+# sharp lines
+bands = edges.split()
+bands[0]
+
+# inverting the colors
+outline = bands[0].point(lambda x: 255 if x<100 else 0)
+print(outline)
+
+# crop image
+outline.crop((10, 200, 300, 400))
+
+# save image to file
+outline.save("file_name.pdf")
 
