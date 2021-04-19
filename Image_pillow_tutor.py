fd2c5de6bc7ebfa04adae66a14f7a357bd2afe1e
rocketpy <dimitr.ushakov@gmail.com>
2021-04-19 21:36:42 +0300

    Update Image_pillow_tutor.py

--- Image_pillow_tutor.py
+++ Image_pillow_tutor.py
@@ -1,1 +1,17 @@
+from PIL import Image
+
+
+# open an some image
+pic = Image.open("img_name.jpg")
+print(pic.size)
+
+
+# get a smaller image
+x, y = pic.size
+x //= 10
+y //= 10
+smaller = pic.resize((x, y))
+print(smaller)
+
+
 
