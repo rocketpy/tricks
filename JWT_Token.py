80cc788b9daa2ec484c8754f3365079357fe4a35
rocketpy <dimitr.ushakov@gmail.com>
2021-04-21 22:44:45 +0300

    Update JWT_Token.py

--- JWT_Token.py
+++ JWT_Token.py
@@ -1,1 +1,14 @@
+# PyJWT - JSON Web Token implementation in Python
 
+# Github - https://github.com/jpadilla/pyjwt
+# pip install PyJWT
+
+import jwt
+
+
+encoded = jwt.encode({"some": "payload"}, "secret", algorithm="HS256")
+print(encoded)
+
+#  Decoding a payload on the server
+jwt.decode(encoded, 'secret', algorithms=['HS256'])
+# {'some': 'payload'}
