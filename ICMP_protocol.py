73ab43abd06822fd7654ca00447fcef9add5d206
rocketpy <dimitr.ushakov@gmail.com>
2021-05-03 22:16:14 +0300

    Update ICMP_protocol.py

--- ICMP_protocol.py
+++ ICMP_protocol.py
@@ -2,6 +2,7 @@
 
 # PyPi:  https://pypi.org/project/icmplib/
 #  pip install icmplib
+#  pip install --upgrade icmplib
 
 """
 Features
@@ -16,3 +17,50 @@ Features
 🍺 Support of all operating systems. Tested on Linux, macOS and Windows.
 🤘 No dependency: icmplib is a pure Python implementation of the ICMP protocol. It does not use any external dependencies.
 """
+
+# For simple use
+from icmplib import ping, multiping, traceroute, resolve, Host, Hop
+
+# For advanced use (sockets)
+from icmplib import ICMPv4Socket, ICMPv6Socket, ICMPRequest, ICMPReply
+
+# Exceptions
+from icmplib import ICMPLibError, NameLookupError, ICMPSocketError
+from icmplib import SocketAddressError, SocketPermissionError
+from icmplib import SocketUnavailableError, SocketBroadcastError, TimeoutExceeded
+from icmplib import ICMPError, DestinationUnreachable, TimeExceeded
+
+
+#  Ping
+# Send ICMP Echo Request packets to a network host.
+ping(address, count=4, interval=1, timeout=2, id=PID, source=None, privileged=True, **kwargs)
+
+"""
+Example
+>>> host = ping('1.1.1.1', count=10, interval=0.2)
+
+>>> host.address            # The IP address of the host that responded
+'1.1.1.1'                   # to the request
+
+>>> host.min_rtt            # The minimum round-trip time
+12.2
+
+>>> host.avg_rtt            # The average round-trip time
+13.2
+
+>>> host.max_rtt            # The maximum round-trip time
+17.6
+
+>>> host.packets_sent       # The number of packets transmitted to the
+10                          # destination host
+
+>>> host.packets_received   # The number of packets sent by the remote
+9                           # host and received by the current host
+
+>>> host.packet_loss        # Packet loss occurs when packets fail to
+0.1                         # reach their destination. Returns a float
+                            # between 0 and 1 (all packets are lost)
+>>> host.is_alive           # Indicates whether the host is reachable
+True
+"""
+
