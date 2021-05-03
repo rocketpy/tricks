1de348bf7235b01fa45c2acc01dc10054006c904
rocketpy <dimitr.ushakov@gmail.com>
2021-05-03 22:14:02 +0300

    Update ICMP_protocol.py

--- ICMP_protocol.py
+++ ICMP_protocol.py
@@ -1,1 +1,18 @@
+#  icmplib - Easily forge ICMP packets and make your own ping and traceroute.
 
+# PyPi:  https://pypi.org/project/icmplib/
+#  pip install icmplib
+
+"""
+Features
+🌳 Ready-to-use: icmplib offers ready-to-use functions such as the most popular ones: ping, multiping and traceroute.
+💎 Modern: This library uses the latest technologies offered by Python 3.6+ and is fully object-oriented.
+🚀 Fast: Each class and function has been designed and optimized to deliver the best performance. Some functions are
+          also multithreaded like the multiping function. You can ping the world in seconds!
+⚡️ Powerful: Use the library without root privileges, set the traffic class of ICMP packets, customize their payload and more!
+🔩 Evolutive: Easily build your own classes and functions with ICMPv4 and ICMPv6 sockets.
+🔥 Seamless integration of IPv6: Use IPv6 the same way you use IPv4. Automatic detection is done without impacting performance.
+🌈 Broadcast support (you must use the ICMPv4Socket class to enable it).
+🍺 Support of all operating systems. Tested on Linux, macOS and Windows.
+🤘 No dependency: icmplib is a pure Python implementation of the ICMP protocol. It does not use any external dependencies.
+"""
