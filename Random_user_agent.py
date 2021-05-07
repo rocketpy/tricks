896b154550653b912afa02781d9bcdbc84353ea5
rocketpy <dimitr.ushakov@gmail.com>
2021-05-07 22:03:08 +0300

    Update Random_user_agent.py

--- Random_user_agent.py
+++ Random_user_agent.py
@@ -5,4 +5,20 @@
 
 
 # Usage
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
+
+
+# you can also import SoftwareEngine, HardwareType, SoftwareType, Popularity from random_user_agent.params
+# you can also set number of user agents required by providing `limit` as parameter
+software_names = [SoftwareName.CHROME.value]
+operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]   
+
+user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+
+# Get list of user agents.
+user_agents = user_agent_rotator.get_user_agents()
+
+# Get Random User Agent String.
+user_agent = user_agent_rotator.get_random_user_agent()
 
