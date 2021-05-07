4c0350d0cf668bdb3e0be843eb7cee7dd7ce52f6
rocketpy <dimitr.ushakov@gmail.com>
2021-05-07 22:01:41 +0300

    Update Random_user_agent.py

--- Random_user_agent.py
+++ Random_user_agent.py
@@ -1,1 +1,8 @@
+#  Random User Agents - A package to get random user agents based filters provided by user
+
+# PyPi: https://pypi.org/project/random-user-agent/
+# Github: https://github.com/Luqman-Ud-Din/random_user_agent
+
+
+# Usage
 
