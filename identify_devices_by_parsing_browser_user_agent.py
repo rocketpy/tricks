086c62af805ce39afa693d88da2b24fa2a3e5328
rocketpy <dimitr.ushakov@gmail.com>
2021-05-08 23:12:24 +0300

    Update identify_devices_by_parsing_browser_user_agent.py

--- identify_devices_by_parsing_browser_user_agent.py
+++ identify_devices_by_parsing_browser_user_agent.py
@@ -1,1 +1,37 @@
+#  user-agents - A library to identify devices (phones, tablets) and their capabilities by parsing browser user agent strings.
+
+
+#  PyPi:  https://pypi.org/project/user-agents/
+
+# pip install user-agents
+
+#  Usage
+
+# Various basic information that can help you identify visitors can be accessed browser, device and os attributes. For example:
+from user_agents import parse
+
+# iPhone's user agent string
+ua_string = 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3'
+user_agent = parse(ua_string)
+
+# Accessing user agent's browser attributes
+user_agent.browser  # returns Browser(family=u'Mobile Safari', version=(5, 1), version_string='5.1')
+user_agent.browser.family  # returns 'Mobile Safari'
+user_agent.browser.version  # returns (5, 1)
+user_agent.browser.version_string   # returns '5.1'
+
+# Accessing user agent's operating system properties
+user_agent.os  # returns OperatingSystem(family=u'iOS', version=(5, 1), version_string='5.1')
+user_agent.os.family  # returns 'iOS'
+user_agent.os.version  # returns (5, 1)
+user_agent.os.version_string  # returns '5.1'
+
+# Accessing user agent's device properties
+user_agent.device  # returns Device(family=u'iPhone', brand=u'Apple', model=u'iPhone')
+user_agent.device.family  # returns 'iPhone'
+user_agent.device.brand # returns 'Apple'
+user_agent.device.model # returns 'iPhone'
+
+# Viewing a pretty string version
+str(user_agent) # returns "iPhone / iOS 5.1 / Mobile Safari 5.1"
 
