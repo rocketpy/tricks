1b87c01a2564f6efb95e18a7de5bc289b15bfdee
rocketpy <dimitr.ushakov@gmail.com>
2021-05-13 23:19:25 +0300

    Update take_webcam_picture.py

--- take_webcam_picture.py
+++ take_webcam_picture.py
@@ -11,3 +11,14 @@ ret, frame = capture.read()
 capture.release()
 
 
+# to display the picture  in Jupyter Notebook 
+"""
+import sys
+from matplotlib import pyplot as plt
+
+
+frameRGB = frame[:, :, ::-1]  # BGR to RGB
+plt.imshow(frameRGB)
+"""
+
+
