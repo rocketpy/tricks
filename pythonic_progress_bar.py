22a72bdb239dea78106e28425498a53f72026f2a
rocketpy <dimitr.ushakov@gmail.com>
2021-05-18 18:31:02 +0300

    Update pythonic_progress_bar.py

--- pythonic_progress_bar.py
+++ pythonic_progress_bar.py
@@ -1,1 +1,15 @@
+# progressist - Minimalist and pythonic progress bar
 
+# PyPI: https://pypi.org/project/progressist/
+# Github: https://github.com/pyrates/progressist
+
+# pip install progressist
+
+# Usage
+from progressist import ProgressBar
+
+bar = ProgressBar(total=mytotalstuff)
+for item in mystuff:
+    # do_stuff
+    bar.update()
+    
