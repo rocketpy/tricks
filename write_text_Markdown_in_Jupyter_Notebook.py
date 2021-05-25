3c61c487cb44ff0cf8c8f972a31481a4139677d6
rocketpy <dimitr.ushakov@gmail.com>
2021-05-25 22:50:45 +0300

    Update and rename write_text_Markdown.py to write_text_Markdown_in_Jupyter_Notebook.py

---
+++ write_text_Markdown_in_Jupyter_Notebook.py
@@ -0,0 +1,28 @@
+#  To write text in Markdown, just use this quick cheatsheet.
+
+# Basically for Jupyter notebook !!!
+
+# Go to Cell > Cell Type > Markdown
+
+"""
+# This is a H1
+## This is a H2
+### This is a H3
+###### This is a H6
+ 
+*italic*
+ 
+**bold**
+ 
+> Quote
+ 
+* Unordered List
+* Unordered List
+ * Unordered subList
+ * Unordered subList
+  
+1. Ordered List
+2. Ordered List
+3. Ordered List
+"""
+
