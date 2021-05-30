fc3092b82f30c4866db342ec8734a02e962ce543
rocketpy <dimitr.ushakov@gmail.com>
2021-05-30 21:19:18 +0300

    Update download_mp3_from_YouTube.py

--- download_mp3_from_YouTube.py
+++ download_mp3_from_YouTube.py
@@ -21,3 +21,17 @@ stream = yt.streams.get_by_itag(251)
 stream.download()
 
 
+# or
+import os
+from pytube import YouTube
+
+
+url = str(input("url: "))
+yt = YouTube(url)
+video = yt.streams.filter(only_audio=True).first()
+downloaded_file = video.download()
+base, ext = os.path.splitext(downloaded_file)
+new_file = base + '.mp3'
+os.rename(downloaded_file, new_file)
+print("Done !")
+
