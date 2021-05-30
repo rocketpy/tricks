e9e0d0aa9d116b1e139f8b770bc1fd9e5944f6a0
rocketpy <dimitr.ushakov@gmail.com>
2021-05-30 20:59:25 +0300

    Update download_YouTube_videos.py

--- download_YouTube_videos.py
+++ download_YouTube_videos.py
@@ -1,1 +1,17 @@
+#  pytube - is a lightweight, Pythonic, dependency-free, library (and command-line utility) for downloading YouTube Videos.
 
+# PyPi: https://pypi.org/project/pytube/
+# Docs: https://pytube.io/en/latest/
+
+# pip install pytube
+
+from pytube import YouTube
+
+YouTube('https://youtu.be/...').streams.first().download()
+yt = YouTube('http://youtube.com/watch?v=')
+yt.streams
+# .filter(progressive=True, file_extension='mp4')
+# .order_by('resolution')
+# .desc()
+# .first()
+# .download()
