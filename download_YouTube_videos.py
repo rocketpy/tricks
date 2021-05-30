2e8b761661235673e716ff95c932c91a64b3ec6a
rocketpy <dimitr.ushakov@gmail.com>
2021-05-30 21:10:22 +0300

    Update download_YouTube_videos.py

--- download_YouTube_videos.py
+++ download_YouTube_videos.py
@@ -15,3 +15,33 @@ yt.streams
 # .desc()
 # .first()
 # .download()
+
+
+# Example, download all the videos from a playlist and saving them with the title from youtube in mp4 and mp4 audio formats.
+"""
+import os
+import re
+import subprocess
+from pytube import YouTube
+from pytube import Playlist
+
+
+path = r'to destination folder'
+playlist_url = '...'
+
+play = Playlist(playlist_url)
+play._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+# print(len(play.video_urls))
+
+for url in play.video_urls:
+    yt = YouTube(url)
+    audio = yt.streams.get_audio_only()
+    audio.download(path)
+    nome = yt.title
+    new_filename=nome+'.mp3'
+    default_filename =nome+'.mp4'
+    ffmpeg = ('ffmpeg -i ' % path default_filename + new_filename)
+    subprocess.run(ffmpeg, shell=True)
+         
+print('Download complete !')
+"""
