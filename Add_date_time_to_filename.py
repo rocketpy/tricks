bf0c96faeaba3b2c56dc8e00656e1115bfa244e4
rocketpy <dimitr.ushakov@gmail.com>
2021-06-03 23:27:41 +0300

    Update Add_date_time_to_filename.py

--- Add_date_time_to_filename.py
+++ Add_date_time_to_filename.py
@@ -1,1 +1,17 @@
 
+
+
+# add Date and Time
+from datetime import datetime
+
+date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
+print(f"filename_{date}")
+
+
+#  add timestamp csv
+import time
+
+for val in data:
+    now = time.strftime('%d-%m-%Y %H:%M:%S')
+    writer.writerow([now, val])
+    
