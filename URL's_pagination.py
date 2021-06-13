2b925dc3a0182778714380b1a7f153be2f3a8d4c
rocketpy <dimitr.ushakov@gmail.com>
2021-06-13 23:11:21 +0300

    Update URL's_pagination.py

--- URL's_pagination.py
+++ URL's_pagination.py
@@ -1,1 +1,6 @@
+# just add this func to your code
 
+def pagination():
+    url = 'https:// ... page={}'
+    urls = [url.format(str(i)) for i in range(1, 357)]  # range(from 1 page to last page)
+    
