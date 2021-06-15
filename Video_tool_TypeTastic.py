af254d61369030ec0a237f83b61a689448226d7b
rocketpy <dimitr.ushakov@gmail.com>
2021-06-15 19:34:22 +0300

    Update Video_tool_TypeTastic.py

--- Video_tool_TypeTastic.py
+++ Video_tool_TypeTastic.py
@@ -1,1 +1,14 @@
+# TypeTastic - Python utility to make creating screencasts easier, and doing live demo's less stressful.
+
+# PyPi: https://pypi.org/project/typetastic/
+
+# pip install typetastic
+
+# TypeTastic uses a Robot to type commands for you. In the simplest form you can pass the commands in as an array.
+import typetastic
+
+
+robot = typetastic.Robot()
+robot.load(['ls', 'echo "Hello World\!"'])
+robot.run()
 
