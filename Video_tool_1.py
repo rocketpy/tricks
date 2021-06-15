a6d8427b00ebeeab592bbca9cd271bfc99940889
rocketpy <dimitr.ushakov@gmail.com>
2021-06-15 19:29:31 +0300

    Update Video_tool_1.py

--- Video_tool_1.py
+++ Video_tool_1.py
@@ -1,1 +1,15 @@
+# castro - Screencasting library, Castro is a library for recording automated screencasts via a simple API.
+
+# PyPi: https://pypi.org/project/castro/
+
+# pip install castro
+
+
+from castro import Castro
+
+
+c = Castro()
+c.start()
+# Do something awesome!
+c.stop()
 
