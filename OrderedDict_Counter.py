899792d813a06d43e5afd4deba59602523838e16
rocketpy <dimitr.ushakov@gmail.com>
2021-06-18 23:27:46 +0300

    Update OrderedDict_Counter.py

--- OrderedDict_Counter.py
+++ OrderedDict_Counter.py
@@ -1,1 +1,8 @@
+# https://docs.python.org/3/library/collections.html
+from collections import OrderedDict, Counter
+
+
+ordered_dict = OrderedDict(a=1, b=2, c=3)
+
+counted = Counter("Hello World !")
 
