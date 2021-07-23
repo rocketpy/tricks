e573408a14e084129ff631394a27edc1185be0de
rocketpy <dimitr.ushakov@gmail.com>
2021-07-23 23:52:55 +0300

    Update Video_editing.py

--- Video_editing.py
+++ Video_editing.py
@@ -3,10 +3,13 @@
 # PyPi: https://pypi.org/project/moviepy/
 # Docs: https://zulko.github.io/moviepy/
 
+# Example Scripts: https://zulko.github.io/moviepy/examples/examples.html
+
 #  pip install moviepy
 
 # Example
 from moviepy.editor import *
+
 
 video = VideoFileClip("myHolidays.mp4").subclip(50,60)
 
@@ -18,3 +21,23 @@ txt_clip = (TextClip("My Holidays 2013",fontsize=70,color='white')
 result = CompositeVideoClip([video, txt_clip]) # Overlay text on video
 result.write_videofile("myHolidays_edited.webm",fps=25) # Many options
 
+
+#  Text with moving letters
+"""
+import numpy as np
+from moviepy.editor import *
+from moviepy.video.tools.segmenting import findObjects
+
+
+# WE CREATE THE TEXT THAT IS GOING TO MOVE, WE CENTER IT.
+
+screensize = (720,460)
+txtClip = TextClip('Cool effect',color='white', font="Amiri-Bold",
+                   kerning = 5, fontsize=100)
+cvc = CompositeVideoClip( [txtClip.set_pos('center')],
+                        size=screensize)
+
+# THE NEXT FOUR FUNCTIONS DEFINE FOUR WAYS OF MOVING THE LETTERS
+
+"""
+ 
