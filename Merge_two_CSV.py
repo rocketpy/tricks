2f03f840b87323bffa0de98b5a1f1f2b29d2ce73
rocketpy <dimitr.ushakov@gmail.com>
2021-07-24 23:23:24 +0300

    Update Merge_two_CSV.py

--- Merge_two_CSV.py
+++ Merge_two_CSV.py
@@ -1,1 +1,12 @@
+import pandas as pd
+  
 
+data_1 = pd.read_csv('file_1.csv')
+data_2 = pd.read_csv('file_2.csv')
+  
+# using merge function by setting how='inner'
+result = pd.merge(data_1, data_2, 
+                   on='LOAN_NO', 
+                   how='inner')
+  
+print(result)
