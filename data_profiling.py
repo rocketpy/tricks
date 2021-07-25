22018c5cd547494ab5f9116315249e1e5945a2bf
rocketpy <dimitr.ushakov@gmail.com>
2021-07-25 23:55:28 +0300

    Update data_profiling.py

--- data_profiling.py
+++ data_profiling.py
@@ -1,1 +1,14 @@
-
+import pandas as pd
+import pandas_profiling
+   
+# read the file
+df = pd.read_csv('Geeks.csv')
+   
+# run the profile report
+profile = df.profile_report(title='Pandas Profiling Report')
+   
+# save the report as html file
+profile.to_file(output_file="pandas_profiling1.html")
+   
+# save the report as json file
+profile.to_file(output_file="pandas_profiling2.json")
