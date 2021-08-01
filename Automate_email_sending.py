e8d26f2422654985c16b714c47be95fdbcf50d54
rocketpy <dimitr.ushakov@gmail.com>
2021-08-01 22:20:56 +0300

    Update Automate_email_sending.py

--- Automate_email_sending.py
+++ Automate_email_sending.py
@@ -1,8 +1,19 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 MY_EMAIL = "..."
 MY_PASSWORD = "..."
 RECIPIENT_ADDRESS = "..."
-
 HOST_ADDRESS = 'smtp.host_address.com'
-HOST_PORT = 587          
+HOST_PORT = 587
+
+
+server = smtplib.SMTP(host=HOST_ADDRESS, port=HOST_PORT)
+server.starttls()
+server.login(MY_EMAIL, MY_PASSWORD)
+
+message = MIMEMultipart()
+
+
