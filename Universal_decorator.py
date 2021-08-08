bd40d1dd656f81094cb063df5fcde2e4725be23e
rocketpy <dimitr.ushakov@gmail.com>
2021-08-08 22:47:48 +0300

    Update Universal_decorator.py

--- Universal_decorator.py
+++ Universal_decorator.py
@@ -1,1 +1,12 @@
+# universal decorator for any number of arguments
 
+def memoize(f):
+    cache = {}
+
+    def decorate(*args, **kwargs):
+        key = (tuple(args), hash(tuple(sorted(kwargs.items()))))
+        if key not in cache:
+            cache[key] = f(*args, **kwargs)
+        return cache[key]
+
+    return decorate
