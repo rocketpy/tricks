ee9e6c58bb67704f9d2288d6ca7bcc08687ce18a
rocketpy <dimitr.ushakov@gmail.com>
2021-08-08 22:51:27 +0300

    Update Memoization_and_currying.py

--- Memoization_and_currying.py
+++ Memoization_and_currying.py
@@ -13,4 +13,20 @@ def memoize(f):
     return decorate
   
   
-  
+# Currying
+# Currying is the transformation of a function from many arguments into a set of functions, each of which is a function from one argument.
+# We can pass some of the arguments to a function and get back a function that expects the rest of the arguments. 
+
+def greet_curried(greeting):
+    def greet(name):
+        print(greeting + ', ' + name)
+    return greet
+
+greet_hello = greet_curried('Hello')
+
+greet_hello('Jack')
+greet_hello('John')
+
+#just call greet_curried
+greet_curried('Hi')('Bob') 
+ 
