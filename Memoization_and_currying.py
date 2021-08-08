90675f916091dd23f4dca23e2e00c07a7ad6a362
rocketpy <dimitr.ushakov@gmail.com>
2021-08-08 22:46:25 +0300

    Update Memoization_and_currying.py

--- Memoization_and_currying.py
+++ Memoization_and_currying.py
@@ -1,1 +1,16 @@
+#  Memoization
+# This is an optimization method in which the result of the function execution is saved and this result is used in the next call. 
 
+def memoize(f):
+    cache = {}
+
+    def decorate(*args):
+        if args in cache:
+            return cache[args]
+        else:
+            cache[args] = f(*args)
+            return cache[args]
+    return decorate
+  
+  
+  
