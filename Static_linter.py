90fc886bfb654031d3945bfe48b3de40161d367e
rocketpy <dimitr.ushakov@gmail.com>
2021-08-10 21:31:19 +0300

    Update Static_linter.py

--- Static_linter.py
+++ Static_linter.py
@@ -1,1 +1,12 @@
+# MyPy - Optional static typing for Python
+
+# Docs: https://mypy.readthedocs.io/en/latest/
+
+# python -m pip install mypy
+
+# to run script use:
+# python -m mypy test_type.py
+
+
+
 
