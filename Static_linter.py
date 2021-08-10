1f75d9d2fb3df4e1d32d5ae47571d7aa9597e7d8
rocketpy <dimitr.ushakov@gmail.com>
2021-08-10 22:06:44 +0300

    Update Static_linter.py

--- Static_linter.py
+++ Static_linter.py
@@ -7,6 +7,10 @@
 # to run script use:
 # python -m mypy test_type.py
 
+# You can teach mypy to detect these kinds of bugs by adding type annotations (also known as type hints).
+# For example, you can teach mypy that greeting both accepts and returns a string like so:
 
+def greeting(name: str) -> str:
+    return 'Hello ' + name
 
 
