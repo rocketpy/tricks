ab38cc8a6d1d157baa60edf80cf0ae5facf07c60
rocketpy <dimitr.ushakov@gmail.com>
2021-09-29 21:52:20 +0300

    Update fake_UserAgent.py

--- fake_UserAgent.py
+++ fake_UserAgent.py
@@ -1,4 +1,9 @@
-#  PyPi:  https://pypi.org/project/fake-useragent/
+# Up to date simple useragent faker with real world database
+
+# PyPi:  https://pypi.org/project/fake-useragent/
+# Github: https://github.com/hellysmile/fake-useragent
+
+# pip install fake-useragent
 
 from selenium import webdriver
 from fake_useragent import UserAgent
@@ -20,3 +25,31 @@ driver.quit()
 from fake_useragent import UserAgent
 
 ua = UserAgent(cache=False)
+
+# Usage
+from fake_useragent import UserAgent
+ua = UserAgent()
+
+ua.ie
+# Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US);
+ua.msie
+# Mozilla/5.0 (compatible; MSIE 10.0; Macintosh; Intel Mac OS X 10_7_3; Trident/6.0)'
+ua['Internet Explorer']
+# Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; GTB7.4; InfoPath.2; SV1; .NET CLR 3.3.69573; WOW64; en-US)
+ua.opera
+# Opera/9.80 (X11; Linux i686; U; ru) Presto/2.8.131 Version/11.11
+ua.chrome
+# Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2'
+ua.google
+# Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/537.13 (KHTML, like Gecko) Chrome/24.0.1290.1 Safari/537.13
+ua['google chrome']
+# Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11
+ua.firefox
+# Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1
+ua.ff
+# Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:15.0) Gecko/20100101 Firefox/15.0.1
+ua.safari
+# Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25
+
+# and the best one, random via real world browser usage statistic
+ua.random
