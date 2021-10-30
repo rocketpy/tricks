5f45b4d9266afab22b27e6e3383b5e3f498ab0c0
rocketpy <dimitr.ushakov@gmail.com>
2021-10-30 21:39:33 +0300

    Update ui_automation_windows.py

--- UI_Automation_Windows/ui_automation_windows.py
+++ UI_Automation_Windows/ui_automation_windows.py
@@ -1,1 +1,8 @@
+#  Do not use 3.7.6 and 3.8.1, comtypes doesn't work in these two versions!!!
+
+# Github: https://github.com/yinkaisheng/Python-UIAutomation-for-Windows
+# UI Automation Specification: https://docs.microsoft.com/en-us/windows/win32/winauto/ui-automation-specification
+
+# How to use uiautomation? run 'automation.py -h' 
+
 
