3c1e82d328cc2d231ea94a393ea1c3b07943a149
rocketpy <dimitr.ushakov@gmail.com>
2021-11-21 17:39:57 +0200

    Update Asynchronous_WebDriver_client.py

--- Asynchronous_WebDriver_client.py
+++ Asynchronous_WebDriver_client.py
@@ -1,1 +1,16 @@
+#  Arsenic - Asynchronous WebDriver client
 
+# PyPi - https://pypi.org/project/arsenic/21.8/
+# Github - https://github.com/HDE/arsenic
+# Docs - https://arsenic.readthedocs.io/en/latest/
+
+# pip install arsenic==21.8
+
+# VeryImportant
+"""
+Warning !!!
+
+While this library is asynchronous, web drivers are not. You must call the APIs in sequence.
+The purpose of this library is to allow you to control multiple web drivers asynchronously or
+to use a web driver in the same thread as an asynchronous web server.
+"""
