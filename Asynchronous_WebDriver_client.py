f2599ba95733165db249cc66525da1bbac2b1f91
rocketpy <dimitr.ushakov@gmail.com>
2021-11-21 17:41:40 +0200

    Update Asynchronous_WebDriver_client.py

--- Asynchronous_WebDriver_client.py
+++ Asynchronous_WebDriver_client.py
@@ -14,3 +14,23 @@ While this library is asynchronous, web drivers are not. You must call the APIs 
 The purpose of this library is to allow you to control multiple web drivers asynchronously or
 to use a web driver in the same thread as an asynchronous web server.
 """
+
+#  Quickstart
+
+# To run a local Firefox instance.
+
+from arsenic import get_session
+from arsenic.browsers import Firefox
+from arsenic.services import Geckodriver
+
+
+async def example():
+    # Runs geckodriver and starts a firefox session
+    async with get_session(Geckodriver(), Firefox()) as session:
+        # go to example.com
+        await session.get('http://example.com')
+        # wait up to 5 seconds to get the h1 element from the page
+        h1 = await session.wait_for_element(5, 'h1')
+        # print the text of the h1 element
+        print(await h1.get_text())
+
