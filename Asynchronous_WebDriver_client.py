0b188fd028c57918544459fafb07306634691af0
rocketpy <dimitr.ushakov@gmail.com>
2021-11-22 22:55:00 +0200

    Update Asynchronous_WebDriver_client.py

--- Asynchronous_WebDriver_client.py
+++ Asynchronous_WebDriver_client.py
@@ -34,3 +34,50 @@ async def example():
         # print the text of the h1 element
         print(await h1.get_text())
 
+
+#  Creating a virtual env
+
+# create a virtual env to install arsenic:
+# python3.6 -m venv env
+
+# env/bin/pip install --upgrade pip
+
+# install arsenic:
+
+# env/bin/pip install --pre arsenic
+
+# create a file named cats.py and insert the following code:
+
+import asyncio
+import sys
+
+from arsenic import get_session, keys, browsers, services
+
+
+if sys.platform.startswith('win'):
+    GECKODRIVER = './geckodriver.exe'
+else:
+    GECKODRIVER = './geckodriver'
+
+
+async def hello_world():
+    service = services.Geckodriver(binary=GECKODRIVER)
+    browser = browsers.Firefox()
+    async with get_session(service, browser) as session:
+        await session.get('https://images.google.com/')
+        search_box = await session.wait_for_element(5, 'input[name=q]')
+        await search_box.send_keys('Cats')
+        await search_box.send_keys(keys.ENTER)
+        await asyncio.sleep(10)
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(hello_world())
+
+
+if __name__ == '__main__':
+    main()
+
+# Save it and in the terminal, run python cats.py
+
