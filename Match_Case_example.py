b6984fde012fe51f460e85b3acf146e1b457ff2d
rocketpy <dimitr.ushakov@gmail.com>
2021-11-29 23:17:57 +0200

    Update Match_Case_example.py

--- Match_Case_example.py
+++ Match_Case_example.py
@@ -1,1 +1,9 @@
+# function for creating a data
+import random as rnd
 
+
+def create_rnd_data():
+    words = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
+         'Hi', 'Hello', 'Welcome', 'World']
+    data = rnd.choices(words, k=500000)
+    return data
