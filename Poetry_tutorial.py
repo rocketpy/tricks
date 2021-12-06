d5f8b3500e407187471909ef008d33baad236ec0
rocketpy <dimitr.ushakov@gmail.com>
2021-12-06 22:21:53 +0200

    Update Poetry_tutorial.py

--- Poetry_tutorial.py
+++ Poetry_tutorial.py
@@ -1,1 +1,13 @@
+# pip install poetry
 
+# by PowerShell
+# (Invoke-WebRequest -Uri https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -UseBasicParsing).Content | python -
+
+# Examples
+# by PIP:  pip install -r requirements.txt
+
+# by Poetry:
+# poetry install
+
+# for update
+# poetry update
