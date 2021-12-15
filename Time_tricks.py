0a8e40e4d374b43a279391477391a5e70de69347
rocketpy <dimitr.ushakov@gmail.com>
2021-12-15 22:54:29 +0200

    Update Time_tricks.py

--- Time_tricks.py
+++ Time_tricks.py
@@ -1,1 +1,10 @@
+# Convert 24 hour time to 12 hour time
 
+from datetime import datetime
+data = datetime.strptime("10:30", "%H:%M")
+data.strftime("%I:%M %p")
+# '10:30 AM'
+
+data = datetime.strptime("22:30", "%H:%M")
+data.strftime("%I:%M %p")
+# '10:30 PM'
