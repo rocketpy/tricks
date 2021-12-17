b72b37baf49c87064dedd30c2c3aad3c86b1a538
rocketpy <dimitr.ushakov@gmail.com>
2021-12-17 21:53:33 +0200

    Update Time_tricks.py

--- Time_tricks.py
+++ Time_tricks.py
@@ -9,6 +9,12 @@ data = datetime.strptime("22:30", "%H:%M")
 data.strftime("%I:%M %p")
 # '10:30 PM'
 
+
+# or
+import time
+
+t = time.strftime("%I:%M %p")
+
 # Converting string to datetime
 from datetime import datetime
 
@@ -20,4 +26,13 @@ from datetime import datetime
 date_string = "2012-12-12 10:10:10"
 print (datetime.fromisoformat(date_string))
 # 2012-12-12 10:10:10
+
+
+# or
+import time
+
+# current time
+date_time = time.strftime("%b %d %Y %-I:%M %p")
+
+
 
