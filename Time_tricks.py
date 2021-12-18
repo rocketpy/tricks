51dfe9c7b0dbfa6ee637ef1eee33f58b8fd837d9
rocketpy <dimitr.ushakov@gmail.com>
2021-12-18 20:56:13 +0200

    Update Time_tricks.py

--- Time_tricks.py
+++ Time_tricks.py
@@ -35,4 +35,23 @@ import time
 date_time = time.strftime("%b %d %Y %-I:%M %p")
 
 
+# Convert a string to date-format
+from datetime import datetime
+
+datetime_object = datetime.strptime('5 July 2020','%d %B %Y')
+s = datetime_object.strftime("%d/%m/%y")
+
+"""
+%a - abbreviated weekday name
+%A - full weekday name
+%b - abbreviated month name
+%B - full month name
+%c - preferred date and time representation
+%C - century number (the year divided by 100, range 00 to 99)
+%d - day of the month (01 to 31)
+%D - same as %m/%d/%y %e - day of the month (1 to 31)
+%g - like %G, but without the century
+%G - 4-digit year corresponding to the ISO week number (see %V)
+%h - same as %b %H - hour, using a 24-hour clock (00 to 23)
+"""
 
