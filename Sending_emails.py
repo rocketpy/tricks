43ae81bc993d349b446e15ac121476dd98e16b65
rocketpy <dimitr.ushakov@gmail.com>
2022-01-13 23:28:50 +0200

    Update Sending_emails.py

--- Sending_emails.py
+++ Sending_emails.py
@@ -1,1 +1,18 @@
+import smtplib
+
+
+sender_email = "...@gmail.com"
+reciever_email = "...@gmail.com"
+password = input('Your email password: ')
+
+subject = "Test"
+body = "Bla bla bla"
+message = f'Subject: {Subject}\n\n{Body}'
+
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login(sender_email, password)
+    smtp.sendmail(sender_email, reciever_email, message)
 
