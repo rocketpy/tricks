ae16d508aa37ba032aa9badf137cf1641599e2f7
rocketpy <dimitr.ushakov@gmail.com>
2022-01-14 23:45:32 +0200

    Update Sending_emails.py

--- Sending_emails.py
+++ Sending_emails.py
@@ -15,4 +15,15 @@ with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
     smtp.ehlo()
     smtp.login(sender_email, password)
     smtp.sendmail(sender_email, reciever_email, message)
+    
+# or
+import smtplib, ssl
+
+
+port = 465
+password = input("Your password: ")
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login("...@gmail.com", password)
 
