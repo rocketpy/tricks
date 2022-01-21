3703a34a50981a74fa1ca9b0d12e1a1cd5c9dffb
rocketpy <dimitr.ushakov@gmail.com>
2022-01-21 23:33:56 +0200

    Update Pyttsx_Voice.py

--- Pyttsx_Voice.py
+++ Pyttsx_Voice.py
@@ -1,1 +1,19 @@
+#  pyttsx3 - Text to Speech (TTS) library for Python 2 and 3. Works without internet connection or delay. 
+#  Supports multiple TTS engines, including Sapi5, nsss, and espeak.
+
+# PyPi: https://pypi.org/project/pyttsx3/
+
+#  pip install pyttsx3
+
+
+
+# Usage :
+
+import pyttsx3
+
+
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()
+
 
