e66a582687ad066e549ef667a4d049ef39db3c09
rocketpy <dimitr.ushakov@gmail.com>
2022-03-10 22:42:50 +0200

    Update Location_by_IP.py

--- Location_by_IP.py
+++ Location_by_IP.py
@@ -1,3 +1,13 @@
+import folium
+import requests
+from pyfiglet import Figlet
+
+
+def get_data(ip='127.0.0.1'):
+    try:
+        response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
+
+
 # requirements
 """
 folium==0.12.1.post1
