3392135ff69b2df6346d108b47421944ac873ac2
rocketpy <dimitr.ushakov@gmail.com>
2022-03-10 22:43:55 +0200

    Update Location_by_IP.py

--- Location_by_IP.py
+++ Location_by_IP.py
@@ -7,6 +7,18 @@ def get_data(ip='127.0.0.1'):
     try:
         response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
 
+        data = {
+            '[IP]': response.get('query'),
+            '[Int prov]': response.get('isp'),
+            '[Org]': response.get('org'),
+            '[Country]': response.get('country'),
+            '[Region Name]': response.get('regionName'),
+            '[City]': response.get('city'),
+            '[ZIP]': response.get('zip'),
+            '[Lat]': response.get('lat'),
+            '[Lon]': response.get('lon')
+        }
+
 
 # requirements
 """
