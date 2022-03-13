551be1c4e0b04e5730ed3b0f7aac754c3527f2c8
rocketpy <dimitr.ushakov@gmail.com>
2022-03-13 22:00:35 +0200

    Update Location_by_IP_2.py

--- Location_by_IP_2.py
+++ Location_by_IP_2.py
@@ -1,7 +1,8 @@
 # get the users ip address
 import socket
+import urllib
 
-print socket.gethostbyname(socket.gethostname())
-
-
+print(socket.gethostbyname(socket.gethostname()))
+response = urllib.urlopen('http://api.hostip.info/get_html.php?   ip=xxx&position=true').read()
+print(response)
 
