43d4023d7d2fcea0e74eb4b21cdad2434d3fbc11
rocketpy <dimitr.ushakov@gmail.com>
2022-03-15 00:05:23 +0200

    Update new_project_2.py

--- new_project_2.py
+++ new_project_2.py
@@ -1,1 +1,19 @@
+#  python-geoip
+
+# pip install python-geoip
+
+# for free MaxMind Geolite2 database you can in addition
+# pip install python-geoip-geolite2
+
+# Docs: https://pythonhosted.org/python-geoip/
+
+# Usage
+from geoip import geolite2
+
+match = geolite2.lookup('17.0.0.1')
+print(match.country)
+# match.continent
+# match.timezone
+# match.subdivisions
+
 
