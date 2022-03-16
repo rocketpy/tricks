5dc2cdbdfb81bd0d2136e12388411a59d75c1220
rocketpy <dimitr.ushakov@gmail.com>
2022-03-16 23:37:25 +0200

    Update Location_by_IP_2.py

--- Location_by_IP_2.py
+++ Location_by_IP_2.py
@@ -13,3 +13,11 @@ url = "http://api.hostip.info/get_html.php?ip=%s&position=true" % ip
 url = "http://api.hostip.info/get_html.php?ip=xxx&position=true".replace("xxx", ip)
 url = "http://api.hostip.info/get_html.php?ip={}&position=true".format(ip)
 
+
+# or 
+from geoip import geolite2
+
+match = geolite2.lookup('10.10.10.10')
+print('Country: '+ match.country)
+print('timezone: '+ match.timezone)
+
