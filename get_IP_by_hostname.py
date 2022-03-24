ac5f040652090914d938f2bc1b5016982e16aa04
rocketpy <dimitr.ushakov@gmail.com>
2022-03-24 23:40:29 +0200

    Update get_IP_by_hostname.py

--- get_IP_by_hostname.py
+++ get_IP_by_hostname.py
@@ -1,1 +1,17 @@
+import socket
 
+
+def get_ip():
+    name = input('URL:  ')
+    try:
+        return f'Hostname: {name}\nIP address: {socket.gethostbyname(name)}'
+    except socket.gaierror as er:
+        return f'Invalid name - {er}'
+
+
+def main():
+    print(get_ip())
+    
+    
+if __name__ == '__main__':
+    main()
