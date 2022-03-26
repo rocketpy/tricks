e5ebbac93a579a9039fc7e82af419f9ae1a68fbe
rocketpy <dimitr.ushakov@gmail.com>
2022-03-26 23:42:26 +0200

    Update get_ip_flask_site.py

--- get_ip_flask_site.py
+++ get_ip_flask_site.py
@@ -5,3 +5,17 @@ from flask import jsonify
 @app.route("/get_my_ip", methods=["GET"])
 def get_my_ip():
     return jsonify({'ip': request.remote_addr}), 200
+
+
+# get client ip flask 
+request.environ['HTTP_X_FORWARDED_FOR']
+
+
+# get flask client ip 
+from flask import request
+
+
+@app.route("/route")
+def get_my_ip():
+    ip = request.remote_addr
+    ...
