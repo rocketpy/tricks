5a1d10c9bb097ec6eb265d4a40b433adfe392e71
rocketpy <dimitr.ushakov@gmail.com>
2022-03-27 23:03:34 +0300

    Update get_IP.py

--- get_IP.py
+++ get_IP.py
@@ -11,3 +11,11 @@ try:
         print(IP)
 except:
     print('Some error !')
+    
+# get public ip address
+from requests import get
+
+
+ip = get('https://api.ipify.org').text
+print(f'My public IP address is: {ip}')
+
