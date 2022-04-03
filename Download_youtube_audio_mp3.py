0f2f63ad089b15605fb2fc72a89a0b6e7981ae20
rocketpy <dimitr.ushakov@gmail.com>
2022-04-03 21:45:10 +0300

    Update Download_youtube_audio_mp3.py

--- Download_youtube_audio_mp3.py
+++ Download_youtube_audio_mp3.py
@@ -1,7 +1,17 @@
 import YouTube from pytube
 
 URL = ""
-
 url = YouTube(URL)
 result = url.streams.filter(only_audio=True)
 result[0].download(/path)
+
+
+# pip install --upgrade youtube-dl
+from __future__ import unicode_literals
+import youtube_dl
+
+URL = ""
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([URL])
+
