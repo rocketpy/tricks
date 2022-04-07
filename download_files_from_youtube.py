ae78704afe04992d659946169ff1dd3275e76575
rocketpy <dimitr.ushakov@gmail.com>
2022-04-07 23:19:07 +0300

    Update download_files_from_youtube.py

--- download_files_from_youtube.py
+++ download_files_from_youtube.py
@@ -1,1 +1,17 @@
+#  mhyt - download files from youtube using simple code
 
+# PyPi: https://pypi.org/project/mhyt/
+# https://github.com/matan-h/mhyt
+
+# pip install mhyt
+
+
+from mhyt import yt_download
+
+
+url = "https://www.youtube.com/..."
+file = "Video.mp4"
+yt_download(url,file)
+
+file = "music.mp3"
+yt_download(url,file,ismusic=True)
