4e425aef9ff76dcb1320c6144655a2574d06cfb3
rocketpy <dimitr.ushakov@gmail.com>
2022-04-14 23:35:22 +0300

    Update Download_youtube_audio_mp3.py

--- Download_youtube_audio_mp3.py
+++ Download_youtube_audio_mp3.py
@@ -33,6 +33,26 @@ with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download(['http://www.youtube.com/...'])
     
     
+# With postprocessors argument
+import youtube_dl
+from __future__ import unicode_literals
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+        'preferredquality': '192'
+    }],
+    'postprocessor_args': [
+        '-ar', '16000'
+    ],
+    'prefer_ffmpeg': True,
+    'keepvideo': True
+}
+    
+    
 # Download video
 # pip install pytube
 
