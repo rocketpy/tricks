2f16009cf8f1783f013393ec3b2f798685c733e5
rocketpy <dimitr.ushakov@gmail.com>
2022-04-16 23:28:20 +0300

    Update download_YouTube_videos.py

--- download_YouTube_videos.py
+++ download_YouTube_videos.py
@@ -79,3 +79,19 @@ for url in play.video_urls:
          
 print('Download complete !')
 """
+
+
+# 
+import os
+import sys
+from pytube import YouTube
+
+
+
+def downloadYoutube(vid_url, path):
+    yt = YouTube(vid_url)
+    yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    yt.download(path)
