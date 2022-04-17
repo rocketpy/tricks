f6e5ad87927bb6936a70ae1f61f8d1ae51c0549d
rocketpy <dimitr.ushakov@gmail.com>
2022-04-17 23:39:03 +0300

    Update set_proxy_auth_globally.py

--- set_proxy_auth_globally.py
+++ set_proxy_auth_globally.py
@@ -1,1 +1,9 @@
+import re
+import urllib2
+import requests
+from requests.auth import HTTPProxyAuth
 
+s = requests.Session()
+
+proxies = {"http": "http://121.132.195.201:8000",
+           "https": "https://134.171.191.202:8000"}
