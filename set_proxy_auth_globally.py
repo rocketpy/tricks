94eb97b2d65ceb2b720aa8e4e35aa057f1273777
rocketpy <dimitr.ushakov@gmail.com>
2022-04-18 23:49:27 +0300

    Update set_proxy_auth_globally.py

--- set_proxy_auth_globally.py
+++ set_proxy_auth_globally.py
@@ -7,3 +7,13 @@ s = requests.Session()
 
 proxies = {"http": "http://121.132.195.201:8000",
            "https": "https://134.171.191.202:8000"}
+
+auth = HTTPProxyAuth("...", "...")
+
+s.proxies = proxies
+
+# set auth params globally
+s.auth = auth
+ext_ip = s.get('http://checkip.dyndns.org')
+
+# print(ext_ip.text)
