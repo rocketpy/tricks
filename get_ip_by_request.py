243a89cc9c6c3ecc5428f3c668bc823f47d780d5
rocketpy <dimitr.ushakov@gmail.com>
2022-04-20 22:53:28 +0300

    Update get_ip_by_request.py

--- get_ip_by_request.py
+++ get_ip_by_request.py
@@ -1,3 +1,9 @@
+# Get location with IP address
+import requests
+
+response = requests.get("https://geolocation-db.com/json/29.100.135.73&position=true").json()
+
+  
 # by curl
 # $ curl 'https://api.ipify.org?format=json'
 
