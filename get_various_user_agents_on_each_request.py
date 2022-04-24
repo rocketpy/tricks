85d0e3ee8503d57e60021cbe237d00294698e4e4
rocketpy <dimitr.ushakov@gmail.com>
2022-04-25 00:42:58 +0300

    Update get_various_user_agents_on_each_request.py

--- get_various_user_agents_on_each_request.py
+++ get_various_user_agents_on_each_request.py
@@ -9,3 +9,20 @@ driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent":
 # driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent": "browser_2"}})
 # driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent": "browser_3"}})
 driver.get('https://www.httpbin.org/headers')
+
+
+ua = UserAgent()
+chrome_options = Options()
+chrome_options.add_argument('no-sandbox')
+chrome_options.add_argument("--start-maximized")
+
+u_agents = []
+
+for agent in u_agents:
+    # user_agent = ua.random
+    # chrome_options.add_argument()
+    driver.execute_cdp_cmd("Network.enable", {})
+    driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent": "browser_1"}})
+    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="C:...")
+    driver.get('https://www...')
+    time.sleep(2)
