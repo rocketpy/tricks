a486711c1e85e9bc3a489e75634cc941f8df4ef4
rocketpy <dimitr.ushakov@gmail.com>
2022-04-25 00:37:34 +0300

    Update get_various_user_agents_on_each_request.py

--- get_various_user_agents_on_each_request.py
+++ get_various_user_agents_on_each_request.py
@@ -1,1 +1,11 @@
+"""
+Need to use: execute_cdp_cmd()
+With execute_cdp_cmd(cmd, cmd_args) we can execute google-chrome-devtools commands using Selenium.
+We can modify the user-agent easily to prevent Selenium from getting detected.
+"""
 
+driver.execute_cdp_cmd("Network.enable", {})
+driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent": "browser_1"}})
+# driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent": "browser_2"}})
+# driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent": "browser_3"}})
+driver.get('https://www.httpbin.org/headers')
