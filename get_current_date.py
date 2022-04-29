b247820034ceada76ca3fd47f217abe6a2c74a96
rocketpy <dimitr.ushakov@gmail.com>
2022-04-29 23:47:59 +0300

    Update get_current_date.py

--- get_current_date.py
+++ get_current_date.py
@@ -1,1 +1,14 @@
+import datetime
 
+year = datetime.datetime.now().year
+# print(year)
+
+# or
+from datetime import date
+
+year = date.today().year
+# print(year)
+# or
+current_date = date.today() 
+# print("Current date: ", current_date)
+# print("Current year:", current_date.year)
