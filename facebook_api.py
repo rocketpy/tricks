b63c05152f8c64306b6dd613df95ca348a7720dc
rocketpy <dimitr.ushakov@gmail.com>
2022-05-04 00:25:43 +0300

    Update facebook_api.py

--- facebook_api.py
+++ facebook_api.py
@@ -1,1 +1,21 @@
+# python-facebook-api - A simple Python wrapper around the Facebook Graph API
+# Now we can use base class GraphApi to get data.
+
+# pip install python-facebook-api
+
+# Giyhub: https://github.com/sns-sdks/python-facebook
+
+#  Usage:
+
+# GraphAPI
+# We can use GraphApi class to communicate with Facebook Graph Api.
+# We can initial GraphApi with three different methods.
+
+# if already have an access token:
+from pyfacebook import GraphAPI
+
+api = GraphAPI(access_token="token")
+
+
+
 
