8a483cc4a49221c68a4e22d21d7e34b09800d91a
rocketpy <dimitr.ushakov@gmail.com>
2022-05-05 23:34:44 +0300

    Update facebook_api.py

--- facebook_api.py
+++ facebook_api.py
@@ -23,6 +23,19 @@ from pyfacebook import GraphAPI
 api = GraphAPI(app_id="id", app_secret="secret", application_only_auth=True)
 
 
+#  to perform an authorization process to a user:
+
+from pyfacebook import GraphAPI
+
+api = GraphAPI(app_id="id", app_secret="secret", oauth_flow=True)
+api.get_authorization_url()
+# ('https://www.facebook.com/dialog/oauth?response_type=code&client_id=id&redirect_uri=https%3A%2F%2Flocalhost%2F&scope=public_profile&state=PyFacebook',
+# 'PyFacebook')
+# let user to do oauth at the browser opened by link.
+# then get the response url
+
+api.exchange_user_access_token(response="url redirected")
+# Now the api will get the user access token.
 
 
 
