0ca6ffd6372c8554331c209474527fc236d6eb4c
rocketpy <dimitr.ushakov@gmail.com>
2022-05-12 23:40:30 +0300

    Update move_the_mouse.py

--- move_the_mouse.py
+++ move_the_mouse.py
@@ -9,3 +9,13 @@ tx, ty = 0, 0
 win32api.mouse_event(win32con.MOUSEEVENTF_MOVE | win32con.MOUSEEVENTF_ABSOLUTE, int(tx/win32api.GetSystemMetrics(0)*65535),
                      int(ty/win32api.GetSystemMetrics(1)*65535) ,0 ,0)
 
+
+# Hook and simulate mouse events on Windows and Linux
+# https://github.com/boppreh/mouse
+
+# pip install mouse
+import mouse
+
+mouse.move(9,45)
+# mouse.drag(start_x, start_y, end_x, end_y, absolute=True, duration=0)
+
