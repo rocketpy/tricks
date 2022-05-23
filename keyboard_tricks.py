70e1e688d4db97d1720975b9d67bae673fd38bf9
rocketpy <dimitr.ushakov@gmail.com>
2022-05-23 23:30:57 +0300

    Update keyboard_tricks.py

--- keyboard_tricks.py
+++ keyboard_tricks.py
@@ -5,6 +5,8 @@
 
 # pip install keyboard
 
+# to clone repo - git clone https://github.com/boppreh/keyboard
+
 import keyboard
 
 
@@ -14,3 +16,18 @@ keyboard.add_hotkey('ctrl+shift+a', print, args=('triggered', 'hotkey'))
 
 # Press PAGE UP then PAGE DOWN to type "foobar".
 keyboard.add_hotkey('page up, page down', lambda: keyboard.write('foobar'))
+
+# Blocks until you press esc.
+keyboard.wait('esc')
+
+# Record events until 'esc' is pressed.
+recorded = keyboard.record(until='esc')
+# Then replay back at three times the speed.
+keyboard.play(recorded, speed_factor=3)
+
+# Type @@ then press space to replace with abbreviation.
+keyboard.add_abbreviation('@@', 'my.long.email@example.com')
+
+# Block forever, like `while True`.
+keyboard.wait()
+
