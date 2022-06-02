c0c04abcbcc95cd391800e8406e1bd13b48018d8
rocketpy <dimitr.ushakov@gmail.com>
2022-06-02 23:55:24 +0300

    Update urllib3_HTTP_client.py

--- urllib3_HTTP_client.py
+++ urllib3_HTTP_client.py
@@ -1,1 +1,10 @@
+# urllib3 - urllib3 is a powerful, user-friendly HTTP client for Python.
 
+
+# easy to use:
+import urllib3
+
+http = urllib3.PoolManager()
+r = http.request('GET', 'http://httpbin.org/robots.txt')
+r.status
+r.data
