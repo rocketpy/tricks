efeecb8792e80c2083d28c9f96d44f55d309dd01
rocketpy <dimitr.ushakov@gmail.com>
2022-06-02 23:56:15 +0300

    Update urllib3_HTTP_client.py

--- urllib3_HTTP_client.py
+++ urllib3_HTTP_client.py
@@ -1,5 +1,15 @@
 # urllib3 - urllib3 is a powerful, user-friendly HTTP client for Python.
 
+# Installing
+# urllib3 can be installed with pip
+
+# python -m pip install urllib3
+
+# from GitHub:
+
+# git clone git://github.com/urllib3/urllib3.git
+# pip install .
+
 
 # easy to use:
 import urllib3
