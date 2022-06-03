72c22914070ba268e92dd7ef2ffce3b11c2a5d3b
rocketpy <dimitr.ushakov@gmail.com>
2022-06-03 23:53:18 +0300

    Update urllib3_HTTP_client.py

--- urllib3_HTTP_client.py
+++ urllib3_HTTP_client.py
@@ -16,5 +16,23 @@ import urllib3
 
 http = urllib3.PoolManager()
 r = http.request('GET', 'http://httpbin.org/robots.txt')
-r.status
-r.data
+print(r.status)
+print(r.data)
+
+
+# Response Content
+# The HTTPResponse object provides status, data, and headers attributes:
+import urllib3
+
+# Making the request (The request function returns HTTPResponse object)
+resp = urllib3.request("GET", "https://httpbin.org/ip")
+
+print(resp.status)
+# 200
+
+print(resp.data)
+# b"{\n  "origin": "104.232.115.37"\n}\n"
+
+print(resp.headers)
+# HTTPHeaderDict({"Content-Length": "32", ...})
+
