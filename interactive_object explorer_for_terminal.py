cf9a0183588232927ef9a9f6d547536e36036ad9
rocketpy <dimitr.ushakov@gmail.com>
2022-06-08 22:47:58 +0300

    Create interactive_object explorer_for_terminal.py

---
+++ interactive_object explorer_for_terminal.py
@@ -0,0 +1,17 @@
+#  Interactive Python Object Explorer
+
+# https://pypi.org/project/objexplore/
+# https://github.com/kylepollina/objexplore
+
+# pip install objexplore
+# or
+# pip install git+https://github.com/kylepollina/objexplore
+
+
+# Usage
+from objexplore import explore
+import rich
+
+explore(rich)
+
+
