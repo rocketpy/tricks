462d4c7f197122d78b358b06b9f0aee57c4db139
rocketpy <dimitr.ushakov@gmail.com>
2022-06-16 23:35:47 +0300

    Create Web3_client.py

---
+++ Web3_client.py
@@ -0,0 +1,20 @@
+# pyweb3 - A Web3 RPC client for Ethereum compatible wallets in Python
+
+# https://github.com/bitlogik/pyWeb3
+# https://pypi.org/project/pyweb3/
+
+#  Installation and requirements: 
+# Works with Python >= 3.6.
+
+# Installation of this library
+python3 -m pip install pyweb3
+
+# From sources, download and run in this directory :
+python3 -m pip install .
+
+"""
+Use:
+Instanciate with pyweb3.Web3Client, then use methods of this object to send RPC queries.
+"""
+
+
