d6382e2d8bf8cf73a5b80524439a6631726b8509
rocketpy <dimitr.ushakov@gmail.com>
2022-06-17 23:53:43 +0300

    Create HTTrack_callbacks.py

---
+++ HTTrack_callbacks.py
@@ -0,0 +1,9 @@
+# HTTrack (http://www.httrack.com) is a GPL'ed software and easy-to-use offline browser utility.
+# The httrack-py library written by Abel Deuring allows to write HTTrack callbacks in Python, or to use the httrack engine in a Python program.
+
+# https://code.google.com/archive/p/httrack-py/
+# https://pypi.org/project/httrack-py/
+
+
+# pip install httrack-py
+
