a69cf6f83278d8f3b34edeb00163e34d026e17a8
rocketpy <dimitr.ushakov@gmail.com>
2022-07-10 23:54:04 +0300

    Update Progress_Meter.py

--- Progress_Meter.py
+++ Progress_Meter.py
@@ -1,1 +1,11 @@
+# tqdm - Fast, Extensible Progress Meter
 
+# https://pypi.org/project/tqdm/
+# tqdm documentation - https://tqdm.github.io/ 
+
+# pip install tqdm
+
+
+from tqdm import tqdm
+for i in tqdm(range(10000)):
+    ...
