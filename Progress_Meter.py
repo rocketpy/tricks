c7d9a8b30860b1c73888c8d68618d8657555f0c9
rocketpy <dimitr.ushakov@gmail.com>
2022-07-11 23:43:42 +0300

    Update Progress_Meter.py

--- Progress_Meter.py
+++ Progress_Meter.py
@@ -9,3 +9,18 @@
 from tqdm import tqdm
 for i in tqdm(range(10000)):
     ...
+
+
+# Usage
+
+# Wrap tqdm() around any iterable:
+
+from tqdm import tqdm
+from time import sleep
+
+text = ""
+
+for char in tqdm(["a", "b", "c", "d"]):
+    sleep(0.25)
+    text = text + char
+    
