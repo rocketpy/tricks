77cc083af4dca8c3c62d5a53e057f083a451f499
rocketpy <dimitr.ushakov@gmail.com>
2022-07-12 23:35:23 +0300

    Update Progress_Meter.py

--- Progress_Meter.py
+++ Progress_Meter.py
@@ -23,4 +23,17 @@ text = ""
 for char in tqdm(["a", "b", "c", "d"]):
     sleep(0.25)
     text = text + char
-    
+
+
+# trange(i) is a special optimised instance of tqdm(range(i)):
+from tqdm import trange
+
+for i in trange(100):
+    sleep(0.01)
+
+# Instantiation outside of the loop allows for manual control over tqdm():
+pbar = tqdm(["a", "b", "c", "d"])
+for char in pbar:
+    sleep(0.25)
+    pbar.set_description("Processing %s" % char)
+
