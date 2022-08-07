91821e807a14ad0373a2aaa907d489bab530baa5
rocketpy <dimitr.ushakov@gmail.com>
2022-08-07 22:42:41 +0300

    Update Pillow_tutor.py

--- Pillow_tutor.py
+++ Pillow_tutor.py
@@ -1,1 +1,11 @@
+# Install
 
+# Windows:
+# python -m venv venv
+# .\venv\Scripts\activate
+# (venv) PS> python -m pip install Pillow
+
+# Linux+macOS:
+# $ python -m venv venv
+# $ source venv/bin/activate
+# (venv) $ python -m pip install Pillow
