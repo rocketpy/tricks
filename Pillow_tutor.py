4bf424b649b4c28069c06a4cea4a533418901912
rocketpy <dimitr.ushakov@gmail.com>
2022-08-08 23:37:08 +0300

    Update Pillow_tutor.py

--- Pillow_tutor.py
+++ Pillow_tutor.py
@@ -9,3 +9,14 @@
 # $ python -m venv venv
 # $ source venv/bin/activate
 # (venv) $ python -m pip install Pillow
+
+
+# Working with image
+from PIL import Image
+
+img_name = "file_name.jpg"
+with Image.open(img_name) as img:
+    img.load()
+
+# type(img)
+# isinstance(img, Image.Image)
