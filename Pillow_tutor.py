450b63d12dd58b4906cac752608da108f32f2c84
rocketpy <dimitr.ushakov@gmail.com>
2022-08-10 23:17:45 +0300

    Update Pillow_tutor.py

--- Pillow_tutor.py
+++ Pillow_tutor.py
@@ -26,3 +26,13 @@ with Image.open(img_name) as img:
 # img.size
 # img.mode
 # isinstance(img, Image.Image)
+
+
+# crop and resize image
+cropped_img = img.crop((250, 100, 500, 900))
+# cropped_img.size
+# cropped_img.show()
+
+low_res_img = cropped_img.resize((cropped_img.width // 4, cropped_img.height // 4))
+low_res_img.show()
+
