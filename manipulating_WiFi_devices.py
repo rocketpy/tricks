7bda81b5061571dc3d11c3e0199579adba27c549
rocketpy <dimitr.ushakov@gmail.com>
2022-12-16 00:55:23 +0200

    Update manipulating_WiFi_devices.py

--- manipulating_WiFi_devices.py
+++ manipulating_WiFi_devices.py
@@ -1,4 +1,16 @@
 # pywifi - A cross-platform module for manipulating WiFi devices.
 
+# https://pypi.org/project/pywifi/
+# https://github.com/awkman/pywifi
+
+# pip install pywifi
 
 
+import pywifi
+
+wifi = pywifi.PyWiFi()
+iface = wifi.interfaces()[0]
+iface.disconnect()
+time.sleep(1)
+assert iface.status() in\
+    [const.IFACE_DISCONNECTED, const.IFACE_INACTIVE]
