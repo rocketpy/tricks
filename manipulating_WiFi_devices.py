77f585596bd447b40ce75805cf2488bfe39b9dcf
rocketpy <dimitr.ushakov@gmail.com>
2022-12-18 00:00:10 +0200

    Update manipulating_WiFi_devices.py

--- manipulating_WiFi_devices.py
+++ manipulating_WiFi_devices.py
@@ -14,3 +14,13 @@ iface.disconnect()
 time.sleep(1)
 assert iface.status() in\
     [const.IFACE_DISCONNECTED, const.IFACE_INACTIVE]
+
+profile = pywifi.Profile()
+profile.ssid = 'testap'
+profile.auth = const.AUTH_ALG_OPEN
+profile.akm.append(const.AKM_TYPE_WPA2PSK)
+profile.cipher = const.CIPHER_TYPE_CCMP
+profile.key = '12345678'
+
+iface.remove_all_network_profiles()
+tmp_profile = iface.add_network_profile(profile)
