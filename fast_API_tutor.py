7c11bdb1efa2d302b4bba5303c6e7fc1a683c045
rocketpy <dimitr.ushakov@gmail.com>
2023-01-07 23:39:37 +0200

    Update fast_API_tutor.py

--- fast_API_tutor.py
+++ fast_API_tutor.py
@@ -44,6 +44,16 @@ class Item(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+
 
 # example taked here: https://habr.com/ru/post/708678/
 from typing import Union
