bf14f47175d5c4c15302436681967c44a204d52d
rocketpy <dimitr.ushakov@gmail.com>
2023-01-11 23:38:56 +0200

    Update fast_API_tutor.py

--- fast_API_tutor.py
+++ fast_API_tutor.py
@@ -111,6 +111,19 @@ app = FastAPI()
 async def read_items():
     return [{"item_id": "Foo"}]
 
+def use_route_names_as_operation_ids(app: FastAPI) -> None:
+    """
+    Simplify operation IDs so that generated API clients have simpler function
+    names.
+
+    Should be called only after all routes have been added.
+    """
+    for route in app.routes:
+        if isinstance(route, APIRoute):
+            route.operation_id = route.name  # in this case, 'read_items'
+
+use_route_names_as_operation_ids(app)
+
 
 
 # example taked here: https://habr.com/ru/post/708678/
