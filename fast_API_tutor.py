b76619a33cd9516d88f3827d7e18964342a8b2aa
rocketpy <dimitr.ushakov@gmail.com>
2023-01-14 21:40:30 +0200

    Update fast_API_tutor.py

--- fast_API_tutor.py
+++ fast_API_tutor.py
@@ -151,7 +151,21 @@ class Item(BaseModel):
     price: float
     tax: Union[float, None] = None
     tags: Set[str] = set()
+        
+@app.post("/items/", response_model=Item, summary="Create an item")
+async def create_item(item: Item):
+    """
+    Create an item with all the information:
 
+    - **name**: each item must have a name
+    - **description**: a long description
+    - **price**: required
+    - **tax**: if the item doesn't have tax, you can omit this
+    - **tags**: a set of unique tag strings for this item
+    \f
+    :param item: User input.
+    """
+    return item
 
 
 # example taked here: https://habr.com/ru/post/708678/
