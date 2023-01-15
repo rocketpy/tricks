aeb335960c1c1b88631a35dd5ba439be392be97d
rocketpy <dimitr.ushakov@gmail.com>
2023-01-15 23:32:04 +0200

    Update fast_API_tutor.py

--- fast_API_tutor.py
+++ fast_API_tutor.py
@@ -168,6 +168,17 @@ async def create_item(item: Item):
     return item
 
 
+# OpenAPI Extensions
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/", openapi_extra={"x-aperture-labs-portal": "blue"})
+async def read_items():
+    return [{"item_id": "portal-gun"}]
+
+
 # example taked here: https://habr.com/ru/post/708678/
 from typing import Union
 from fastapi import FastAPI
