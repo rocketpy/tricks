254222e030523f803d82adef51d6e28d66489fc6
rocketpy <dimitr.ushakov@gmail.com>
2023-01-19 21:00:21 +0200

    Update fast_API_tutor.py

--- fast_API_tutor.py
+++ fast_API_tutor.py
@@ -283,6 +283,18 @@ class Item(BaseModel):
     },
 )
 
+async def create_item(request: Request):
+    raw_body = await request.body()
+    try:
+        data = yaml.safe_load(raw_body)
+    except yaml.YAMLError:
+        raise HTTPException(status_code=422, detail="Invalid YAML")
+    try:
+        item = Item.parse_obj(data)
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=e.errors())
+    return item
+
 
 # example taked here: https://habr.com/ru/post/708678/
 from typing import Union
