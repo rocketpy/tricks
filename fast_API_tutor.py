24380732fc69b21cdbb69bab74ed9d341eefca5c
rocketpy <dimitr.ushakov@gmail.com>
2023-01-20 23:47:05 +0200

    Update fast_API_tutor.py

--- fast_API_tutor.py
+++ fast_API_tutor.py
@@ -311,6 +311,16 @@ async def upsert_item(
     item_id: str,
     name: Union[str, None] = Body(default=None),
     size: Union[int, None] = Body(default=None),
+):
+    if item_id in items:
+        item = items[item_id]
+        item["name"] = name
+        item["size"] = size
+        return item
+    else:
+        item = {"name": name, "size": size}
+        items[item_id] = item
+        return JSONResponse(status_code=status.HTTP_201_CREATED, content=item)
 
 
 # example taked here: https://habr.com/ru/post/708678/
