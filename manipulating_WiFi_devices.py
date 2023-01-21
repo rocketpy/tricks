4855ed06a7f8160ac06c5dc99774dfae66f45226
rocketpy <dimitr.ushakov@gmail.com>
2023-01-21 23:55:13 +0200

    Update manipulating_WiFi_devices.py

--- manipulating_WiFi_devices.py
+++ manipulating_WiFi_devices.py
@@ -6,6 +6,23 @@
 # pip install pywifi
 
 
+# Example:
+
+import pywifi
+
+profile = pywifi.Profile()
+profile.ssid = 'testap'
+profile.auth = const.AUTH_ALG_OPEN
+profile.akm.append(const.AKM_TYPE_WPA2PSK)
+profile.cipher = const.CIPHER_TYPE_CCMP
+profile.key = '12345678'
+
+wifi = pywifi.PyWiFi()
+iface = wifi.interfaces()[0]
+profile = iface.add_network_profile(profile)
+iface.connect(profile)
+
+
 import pywifi
 
 wifi = pywifi.PyWiFi()
