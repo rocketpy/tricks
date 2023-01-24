d88e9d283f1d7cb404dd5685dbefb49441d14ca8
rocketpy <dimitr.ushakov@gmail.com>
2023-01-24 23:55:42 +0200

    Update fast_API_tutor.py

--- fast_API_tutor.py
+++ fast_API_tutor.py
@@ -321,6 +321,30 @@ async def upsert_item(
         item = {"name": name, "size": size}
         items[item_id] = item
         return JSONResponse(status_code=status.HTTP_201_CREATED, content=item)
+    
+
+# Using the jsonable_encoder in a Response
+from datetime import datetime
+from typing import Union
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    title: str
+    timestamp: datetime
+    description: Union[str, None] = None
+
+
+app = FastAPI()
+
+
+@app.put("/items/{id}")
+def update_item(id: str, item: Item):
+    json_compatible_item_data = jsonable_encoder(item)
+    return JSONResponse(content=json_compatible_item_data)
 
 
 # example taked here: https://habr.com/ru/post/708678/
