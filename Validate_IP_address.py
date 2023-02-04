defae6231dae575ba08462555c134c4c51c6cafd
rocketpy <dimitr.ushakov@gmail.com>
2023-02-05 00:54:08 +0200

    Update Validate_IP_address.py

--- Validate_IP_address.py
+++ Validate_IP_address.py
@@ -1,22 +1,24 @@
 class Validator(object):
-   def validIPAddress(self, IP):
-      """
-      :type IP: str
-      :rtype: str
-      """
-      def isIPv4(s):
-         try: return str(int(s)) == s and 0 <= int(s) <= 255
-         except: return False
-      def isIPv6(s):
-         if len(s) > 4:
-            return False
-         try : return int(s, 16) >= 0 and s[0] != '-'
-         except:
-            return False
-         if IP.count(".") == 3 and all(isIPv4(i) for i in IP.split(".")):
-         return "IPv4"
-      if IP.count(":") == 7 and all(isIPv6(i) for i in IP.split(":")):
-         return "IPv6"
-      return "Neither"
-ob = Solution()
-print(ob.validIPAddress("172.16.254.1"))
+    def valid_IP_address(self, IP):
+        """
+        :type IP: str
+        :rtype: str
+        """
+        def isIPv4(s):
+            try: return str(int(s)) == s and 0 <= int(s) <= 255
+            except: return False
+        def isIPv6(s):
+            if len(s) > 4:
+                return False
+            try : return int(s, 16) >= 0 and s[0] != '-'
+            except:
+                return False
+        if IP.count(".") == 3 and all(isIPv4(i) for i in IP.split(".")):
+            return "IPv4"
+        if IP.count(":") == 7 and all(isIPv6(i) for i in IP.split(":")):
+            return "IPv6"
+      return "None"
+
+res = Validator()
+
+print(res.valid_IP_address("132.17.255.1"))
