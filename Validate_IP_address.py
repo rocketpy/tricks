a53a7bd51cfd849af35f2483e95fac1a0c185425
rocketpy <dimitr.ushakov@gmail.com>
2023-02-04 23:59:32 +0200

    Update Validate_IP_address.py

--- Validate_IP_address.py
+++ Validate_IP_address.py
@@ -13,3 +13,10 @@ class Validator(object):
          try : return int(s, 16) >= 0 and s[0] != '-'
          except:
             return False
+         if IP.count(".") == 3 and all(isIPv4(i) for i in IP.split(".")):
+         return "IPv4"
+      if IP.count(":") == 7 and all(isIPv6(i) for i in IP.split(":")):
+         return "IPv6"
+      return "Neither"
+ob = Solution()
+print(ob.validIPAddress("172.16.254.1"))
