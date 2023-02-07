3f4b0da4fda470cd2a74758bf293afecc0e3d41b
rocketpy <dimitr.ushakov@gmail.com>
2023-02-08 00:00:07 +0200

    Update extracting_MAC_address.py

--- extracting_MAC_address.py
+++ extracting_MAC_address.py
@@ -1,3 +1,10 @@
 import uuid
 
 print (hex(uuid.getnode()))
+
+# or
+import uuid
+# after each 2 digits, join elements of getnode().
+print ("The formatted MAC address is : ", end="")
+print (':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff)
+for elements in range(0,2*6,2)][::-1]))
