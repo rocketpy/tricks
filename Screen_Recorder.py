98f441f6a542e309571e97078353edb531f04524
rocketpy <dimitr.ushakov@gmail.com>
2023-03-08 23:50:51 +0200

    Update Screen_Recorder.py

--- Screen_Recorder.py
+++ Screen_Recorder.py
@@ -48,4 +48,12 @@ fps = 12.0
 out = cv2.VideoWriter("output.avi", fourcc, fps, (SCREEN_SIZE))
 record_seconds = 10
 
+for i in range(int(record_seconds * fps)):
+    img = pyautogui.screenshot()
+    frame = np.array(img)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    out.write(frame)
+    cv2.imshow("screenshot", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
 
