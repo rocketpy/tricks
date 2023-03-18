c0531f8217c73f88f311583bb77f1500886116e5
rocketpy <dimitr.ushakov@gmail.com>
2023-03-18 23:56:48 +0200

    Update Extract_frames_from_Video.py

--- Extract_frames_from_Video.py
+++ Extract_frames_from_Video.py
@@ -72,3 +72,17 @@ import numpy as np
 from datetime import timedelta
 from moviepy.editor import VideoFileClip
 
+
+SAVING_FRAMES_PER_SECOND = 10
+
+def format_timedelta(td):
+    result = str(td)
+    
+    try:
+        result, ms = result.split(".")
+    except ValueError:
+        return (result + ".00").replace(":", "-")
+
+    ms = int(ms)
+    ms = round(ms / 1e4)
+    return f"{result}.{ms:02}".replace(":", "-")
