34bf3865bde9f57961d1a166657423656308cf82
rocketpy <dimitr.ushakov@gmail.com>
2023-03-22 23:46:01 +0200

    Update Extract_Audio_from_video.py

--- Extract_Audio_from_video.py
+++ Extract_Audio_from_video.py
@@ -7,3 +7,10 @@
 import os
 import sys
 import subprocess
+
+
+def convert_video_to_audio_ffmpeg(video_file, output_ext="mp3"):
+    filename, ext = os.path.splitext(video_file)
+    subprocess.call(["ffmpeg", "-y", "-i", video_file, f"{filename}.{output_ext}"], 
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.STDOUT)
