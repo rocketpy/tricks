63934279839e04bd7c440518287b6ee2ac02f08b
rocketpy <dimitr.ushakov@gmail.com>
2023-03-27 23:55:36 +0300

    Update Compress_images.py

--- Compress_images.py
+++ Compress_images.py
@@ -1,1 +1,13 @@
+# pip install Pillow
 
+import os
+from PIL import Image
+
+
+# print the file size
+def get_size_format(b, factor=1024, suffix="B"):
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+        if b < factor:
+            return f"{b:.2f}{unit}{suffix}"
+        b /= factor
+    return f"{b:.2f}Y{suffix}"
