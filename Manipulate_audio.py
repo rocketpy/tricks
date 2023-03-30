8e3d8be3390ef0ea250fb19e782d3e9e62462152
rocketpy <dimitr.ushakov@gmail.com>
2023-03-30 23:46:46 +0300

    Update Manipulate_audio.py

--- Manipulate_audio.py
+++ Manipulate_audio.py
@@ -1,11 +1,13 @@
 # pydub - Manipulate audio with an simple and easy high level interface
 
 # http://pydub.com/
-
 # pip install pydub
 
-# Open a WAV file
+
 from pydub import AudioSegment
 
+# Open a WAV file
 song = AudioSegment.from_wav("song_name.wav")
 
+# mp3
+song = AudioSegment.from_mp3("song_name.mp3")
