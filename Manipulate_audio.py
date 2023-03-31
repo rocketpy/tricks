527ede45dea0a8049a9a804eb734198a242626fc
rocketpy <dimitr.ushakov@gmail.com>
2023-03-31 23:47:33 +0300

    Update Manipulate_audio.py

--- Manipulate_audio.py
+++ Manipulate_audio.py
@@ -11,3 +11,18 @@ song = AudioSegment.from_wav("song_name.wav")
 
 # mp3
 song = AudioSegment.from_mp3("song_name.mp3")
+
+# others
+ogg_version = AudioSegment.from_ogg("never_gonna_give_you_up.ogg")
+flv_version = AudioSegment.from_flv("never_gonna_give_you_up.flv")
+mp4_version = AudioSegment.from_file("never_gonna_give_you_up.mp4", "mp4")
+wma_version = AudioSegment.from_file("never_gonna_give_you_up.wma", "wma")
+aac_version = AudioSegment.from_file("never_gonna_give_you_up.aiff", "aac")
+
+
+# Take a slice audio
+
+# pydub does things in miliseconds
+ten_seconds = 10 * 1000
+first_10_seconds = song[:10000]
+last_5_seconds = song[-5000:]
