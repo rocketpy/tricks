49b2d6a3b2719247e59b8f4c38694440c7654090
rocketpy <dimitr.ushakov@gmail.com>
2023-04-04 23:47:20 +0300

    Update Manipulate_audio.py

--- Manipulate_audio.py
+++ Manipulate_audio.py
@@ -68,3 +68,9 @@ from pydub import AudioSegment
 
 video_dir = '/home/johndoe/downloaded_videos/'  # Path where the videos are located
 extension_list = ('*.mp4', '*.flv')
+
+os.chdir(video_dir)
+for extension in extension_list:
+    for video in glob.glob(extension):
+        mp3_filename = os.path.splitext(os.path.basename(video))[0] + '.mp3'
+        AudioSegment.from_file(video).export(mp3_filename, format='mp3')
