4abbfeabadc6e8e32835a691f5b4416fad0f4643
rocketpy <dimitr.ushakov@gmail.com>
2023-04-08 23:31:17 +0300

    Update Manipulate_audio.py

--- Manipulate_audio.py
+++ Manipulate_audio.py
@@ -128,3 +128,10 @@ model.add(STFT(n_fft=2048, win_length=2018, hop_length=1024,
                window_name=None, pad_end=False,
                input_data_format='channels_last', output_data_format='channels_last',
                input_shape=input_shape))
+
+model.add(Magnitude())
+model.add(MagnitudeToDecibel())  # these three layers can be replaced with get_stft_magnitude_layer()
+# Alternatively, you may want to use a melspectrogram layer
+# melgram_layer = get_melspectrogram_layer()
+# or log-frequency layer
+# log_stft_layer = get_log_frequency_spectrogram_layer() 
