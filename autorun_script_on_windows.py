ce89751e1c914d32be6fc3e8fd64c8fcfa8cf603
rocketpy <dimitr.ushakov@gmail.com>
2023-05-06 23:39:15 +0300

    Update autorun_script_on_windows.py

--- autorun_script_on_windows.py
+++ autorun_script_on_windows.py
@@ -3,4 +3,16 @@ import winreg as reg
             
  
 def AddToRegistry():
+    # then __file__ will be
+    # c:\users\current_user\desktop
     pth = os.path.dirname(os.path.realpath(__file__))
+    s_name = "file_name.py"     
+    address=os.join(pth, s_name)
+    key = HKEY_CURRENT_USER
+    key_value = "Software\Microsoft\Windows\CurrentVersion\Run"
+    open = reg.OpenKey(key, key_value, 0, reg.KEY_ALL_ACCESS)
+    reg.SetValueEx(open, "any_name", 0, reg.REG_SZ,address)
+    reg.CloseKey(open)
+
+if __name__=="__main__":
+    AddToRegistry()
