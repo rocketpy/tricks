dde7277bc921305ccd11afa22461ae6bc84d2210
rocketpy <dimitr.ushakov@gmail.com>
2023-05-07 23:51:50 +0300

    Update autorun_script_on_windows.py

--- autorun_script_on_windows.py
+++ autorun_script_on_windows.py
@@ -1,3 +1,14 @@
+# py2exe - Build standalone executables for Windows
+
+# https://pypi.org/project/py2exe/
+# Docs - https://www.py2exe.org/
+# https://github.com/py2exe/py2exe
+# Tutorial - https://www.py2exe.org/index.cgi/Tutorial
+
+# pip install py2exe
+
+
+# Example
 import os
 import winreg as reg
             
