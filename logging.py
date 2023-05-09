babdeae8035e2194d3dd1433ccd9deb67d8448c5
rocketpy <dimitr.ushakov@gmail.com>
2023-05-09 23:04:22 +0300

    Update logging.py

--- logging.py
+++ logging.py
@@ -3,3 +3,19 @@ import logging
 
 def main():
     logging.basicConfig(filename ='app.log', level = logging.ERROR)
+    
+    # Calls variables
+    hostname = 'www...com'
+    item = 'spam'
+    filename = 'data.csv'
+    mode = 'r'
+      
+    # logging calls
+    logging.critical('Host %s unknown', hostname)
+    logging.error("Couldn't find %r", item)
+    logging.warning('Feature is deprecated')
+    logging.info('Opening file %r, mode = %r', filename, mode)
+    logging.debug('Got here')
+      
+if __name__ == '__main__':
+    main()
