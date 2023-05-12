5ba6c9b522daeb3fb7d669afcb531331fb41e2e2
rocketpy <dimitr.ushakov@gmail.com>
2023-05-12 23:52:16 +0300

    Update py_ChatGPT.py

--- py_ChatGPT.py
+++ py_ChatGPT.py
@@ -1,1 +1,15 @@
+# pyChatGPT - An unofficial Python wrapper for OpenAI's ChatGPT API
 
+# https://pypi.org/project/pyChatGPT/
+# https://github.com/terry3041/pyChatGPT
+
+# pip install pyChatGPT
+
+"""
+Features:
+ Cloudflare's anti-bot protection bypass using undetected_chromedriver
+ OpenAI / Google / Microsoft login support (experimental)
+ Captcha solvers support (2Captcha, PyPasser)
+ Headless machines support
+ Proxy support (only without basic auth)
+"""
