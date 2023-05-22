4ed3122d837854900575f52b040932524c6dd2e2
rocketpy <dimitr.ushakov@gmail.com>
2023-05-22 23:46:32 +0300

    Update Google_Bard_API.py

--- Google_Bard_API.py
+++ Google_Bard_API.py
@@ -1,1 +1,11 @@
+# Google Bard API
 
+# https://github.com/dsdanielpark/Bard-API
+# pip install bardapi
+
+# Simple Usage
+from bardapi import Bard
+
+token = 'xxxxxxxxxx'
+bard = Bard(token=token)
+bard.get_answer(".....")['content']
