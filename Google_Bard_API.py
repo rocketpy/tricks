3802de8dd1f84d28d3e42ee85875319f4d39194a
rocketpy <dimitr.ushakov@gmail.com>
2023-05-26 23:35:28 +0300

    Update Google_Bard_API.py

--- Google_Bard_API.py
+++ Google_Bard_API.py
@@ -51,3 +51,19 @@ os.environ['_BARD_API_KEY'] = 'xxxxxxxxxxx'
 # token='xxxxxxxxxxx'
 
 session = requests.Session()
+session.headers = {
+            "Host": "bard.google.com",
+            "X-Same-Domain": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+            "Origin": "https://bard.google.com",
+            "Referer": "https://bard.google.com/",
+        }
+session.cookies.set("__Secure-1PSID", os.getenv("_BARD_API_KEY")) 
+# session.cookies.set("__Secure-1PSID", token) 
+
+bard = Bard(token=token, session=session, timeout=30)
+bard.get_answer(".....")['content']
+
+# Continued conversation without set new session
+bard.get_answer("What is my last prompt??")['content']
