be37ccfecd8fee0d5de89cd1ec84e6959a4d98f8
rocketpy <dimitr.ushakov@gmail.com>
2023-06-07 23:42:27 +0300

    Update Tinygrad_deep_learning_framework.py

--- Tinygrad_deep_learning_framework.py
+++ Tinygrad_deep_learning_framework.py
@@ -2,3 +2,14 @@
 
 # https://github.com/geohot/tinygrad
 
+# Laziness
+DEBUG=3 OPTLOCAL=1 python3 -c "from tinygrad.tensor import Tensor;
+N = 1024; a, b = Tensor.rand(N, N), Tensor.rand(N, N);
+c = (a.reshape(N, 1, N) * b.permute(1,0).reshape(1, N, N)).sum(axis=2);
+print((c.numpy() - (a.numpy() @ b.numpy())).mean())"
+
+"""
+Neural networks
+As it turns out, 90% of what you need for neural networks are a decent autograd/tensor library.
+Throw in an optimizer, a data loader, and some compute, and you have all you need.
+"""
