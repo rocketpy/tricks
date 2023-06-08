32a962f77da113ff1b0180f22c425e4db6bff589
rocketpy <dimitr.ushakov@gmail.com>
2023-06-08 23:52:33 +0300

    Update Tinygrad_deep_learning_framework.py

--- Tinygrad_deep_learning_framework.py
+++ Tinygrad_deep_learning_framework.py
@@ -13,3 +13,14 @@ Neural networks
 As it turns out, 90% of what you need for neural networks are a decent autograd/tensor library.
 Throw in an optimizer, a data loader, and some compute, and you have all you need.
 """
+
+from tinygrad.tensor import Tensor
+import tinygrad.nn.optim as optim
+
+class TinyBobNet:
+  def __init__(self):
+    self.l1 = Tensor.uniform(784, 128)
+    self.l2 = Tensor.uniform(128, 10)
+
+  def forward(self, x):
+    return x.dot(self.l1).relu().dot(self.l2).log_softmax()
