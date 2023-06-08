be772659d1fa71efcec6b7d2c7bf66312b40a05a
rocketpy <dimitr.ushakov@gmail.com>
2023-06-08 23:53:57 +0300

    Update Tinygrad_deep_learning_framework.py

--- Tinygrad_deep_learning_framework.py
+++ Tinygrad_deep_learning_framework.py
@@ -24,3 +24,13 @@ class TinyBobNet:
 
   def forward(self, x):
     return x.dot(self.l1).relu().dot(self.l2).log_softmax()
+  
+model = TinyBobNet()
+optim = optim.SGD([model.l1, model.l2], lr=0.001)
+
+# ... complete data loader here
+out = model.forward(x)
+loss = out.mul(y).mean()
+optim.zero_grad()
+loss.backward()
+optim.step()
