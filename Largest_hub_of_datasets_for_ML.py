57633671982dd63a6be6eec65c8e3b3940211cf1
rocketpy <dimitr.ushakov@gmail.com>
2023-06-09 23:46:38 +0300

    Create Largest_hub_of_datasets_for_ML.py

---
+++ Largest_hub_of_datasets_for_ML.py
@@ -0,0 +1,11 @@
+# The largest hub of ready-to-use datasets for ML models with fast, easy-to-use and efficient data manipulation tools
+
+# https://github.com/huggingface/datasets
+# huggingface.co/docs/datasets
+
+# Installation
+# pip install datasets
+
+# With conda
+# conda install -c huggingface -c conda-forge datasets
+
