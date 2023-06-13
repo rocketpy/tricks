f789b7ae024f3a3ed62201e72922f52f0d1d140a
rocketpy <dimitr.ushakov@gmail.com>
2023-06-14 00:10:08 +0300

    Update Bard_chatbot_API.py

--- Bard_chatbot_API.py
+++ Bard_chatbot_API.py
@@ -2,10 +2,21 @@
 
 # https://github.com/acheong08/Bard
 
-
 # Authentication
 # Go to https://bard.google.com/
 
-
 # pip3 install --upgrade GoogleBard
 
+
+# Usage
+"""
+$ python3 -m Bard -h
+usage: Bard.py [-h] --session SESSION
+options:
+  -h, --help         show this help message and exit
+  --session SESSION  __Secure-1PSID cookie.
+"""
+# Quick mode
+# $ export BARD_QUICK="true"
+# $ export BARD_SESSION="<__Secure-1PSID>"
+# $ python3 -m Bard
