4560079144f49de0e2d49b08c43e84d88a8766ac
rocketpy <dimitr.ushakov@gmail.com>
2023-06-20 23:43:09 +0300

    Create GPT_engineer.py

---
+++ GPT_engineer.py
@@ -0,0 +1,11 @@
+# GPT Engineer - Specify what you want it to build, the AI asks for clarification, and then builds it.
+
+# https://github.com/AntonOsika/gpt-engineer
+
+# pip install gpt-engineer
+
+# For development:
+# git clone git@github.com:AntonOsika/gpt-engineer.git
+# cd gpt-engineer
+# make install
+# source venv/bin/activate
