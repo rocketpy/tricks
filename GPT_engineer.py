c58c4ead204a58d1ff77ec4883595dfe3746785d
rocketpy <dimitr.ushakov@gmail.com>
2023-06-21 23:09:50 +0300

    Update GPT_engineer.py

--- GPT_engineer.py
+++ GPT_engineer.py
@@ -9,3 +9,15 @@
 # cd gpt-engineer
 # make install
 # source venv/bin/activate
+
+# With an api key that has GPT4 access run:
+# export OPENAI_API_KEY=[your api key]
+
+# Run:
+# Create an empty folder. If inside the repo, you can run:
+# cp -r projects/example/ projects/my-new-project
+# Fill in the main_prompt file in your new folder
+# Run: gpt-engineer projects/my-new-project
+
+# Results
+# Check the generated files in projects/my-new-project/workspace
