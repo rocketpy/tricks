398de82cf80ec7e40c128bdaa1bbb9cee0c80bd2
rocketpy <dimitr.ushakov@gmail.com>
2023-06-23 23:53:35 +0300

    Update Bard_chatbot_API.py

--- Bard_chatbot_API.py
+++ Bard_chatbot_API.py
@@ -16,13 +16,13 @@ options:
   -h, --help         show this help message and exit
   --session SESSION  __Secure-1PSID cookie.
 """
+
 # Quick mode
 # $ export BARD_QUICK="true"
 # $ export BARD_SESSION="<__Secure-1PSID>"
 # $ python3 -m Bard
 
 # Example bash shortcut:
-
 # USAGE1: bard QUESTION
 # USAGE2: echo "QUESTION" | bard
 bard () {
@@ -30,3 +30,14 @@ bard () {
 	export BARD_SESSION=<REDACTED>.
 	python3 -m Bard "${@:-$(</dev/stdin)}" | tail -n+7
 }
+
+
+# Developer Documentation
+from os import environ
+from Bard import Chatbot
+
+token = environ.get("BARD_TOKEN")
+
+chatbot = Chatbot(token)
+
+chatbot.ask("Hello, how are you?")
