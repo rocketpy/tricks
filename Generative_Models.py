7ae9fb56795646731ece719990bee96013560375
rocketpy <dimitr.ushakov@gmail.com>
2023-06-24 23:53:58 +0300

    Update Generative_Models.py

--- Generative_Models.py
+++ Generative_Models.py
@@ -1,1 +1,7 @@
+# Generative Models by Stability AI
 
+# https://github.com/Stability-AI/generative-models
+
+# General Philosophy
+# Modularity is king. This repo implements a config-driven approach where we build and combine submodules by
+# calling instantiate_from_config() on objects defined in yaml configs. See configs/ for many examples.
