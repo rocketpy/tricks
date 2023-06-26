4c12df755fa5e3c1891f6bba647bc6bd8dafa69a
rocketpy <dimitr.ushakov@gmail.com>
2023-06-26 23:53:53 +0300

    Update Generative_Models.py

--- Generative_Models.py
+++ Generative_Models.py
@@ -5,3 +5,20 @@
 # General Philosophy
 # Modularity is king. This repo implements a config-driven approach where we build and combine submodules by
 # calling instantiate_from_config() on objects defined in yaml configs. See configs/ for many examples.
+
+# Installation:
+git clone git@github.com:Stability-AI/generative-models.git
+cd generative-models
+
+# Setting up the virtualenv
+# This is assuming you have navigated to the generative-models root after cloning it!!!
+
+NOTE: This is tested under python3.8 and python3.10. For other python versions, you might encounter version conflicts!!!
+
+# PyTorch 1.13
+
+# install required packages from pypi
+python3 -m venv .pt1
+source .pt1/bin/activate
+pip3 install wheel
+pip3 install -r requirements_pt13.txt
