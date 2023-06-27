a9400bff5bec647c81da40bc4a2e124604e16d7f
rocketpy <dimitr.ushakov@gmail.com>
2023-06-28 00:05:03 +0300

    Update Generative_Models.py

--- Generative_Models.py
+++ Generative_Models.py
@@ -13,10 +13,9 @@ cd generative-models
 # Setting up the virtualenv
 # This is assuming you have navigated to the generative-models root after cloning it!!!
 
-NOTE: This is tested under python3.8 and python3.10. For other python versions, you might encounter version conflicts!!!
+# NOTE: This is tested under python3.8 and python3.10. For other python versions, you might encounter version conflicts!!!
 
 # PyTorch 1.13
-
 # install required packages from pypi
 python3 -m venv .pt1
 source .pt1/bin/activate
@@ -24,10 +23,13 @@ pip3 install wheel
 pip3 install -r requirements_pt13.txt
 
 
-PyTorch 2.0
-
+# PyTorch 2.0
 # install required packages from pypi
 python3 -m venv .pt2
 source .pt2/bin/activate
 pip3 install wheel
 pip3 install -r requirements_pt2.txt
+
+# Dataset Handling
+example = {"jpg": x,  # this is a tensor -1...1 chw
+           "txt": "a beautiful image"}
