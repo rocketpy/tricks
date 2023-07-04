a03c9a8064235abaec00816e1f7aae16da5322e6
rocketpy <dimitr.ushakov@gmail.com>
2023-07-04 23:25:26 +0300

    Create Phonenumbers_lib.py

---
+++ Phonenumbers_lib.py
@@ -0,0 +1,6 @@
+# Python version of Google's common library for parsing, formatting, storing and validating international phone numbers.
+
+# https://pypi.org/project/phonenumbers/
+# https://github.com/daviddrysdale/python-phonenumbers
+
+# pip install phonenumbers
