5057e304bb6a03797a64a25824b5366ea7349a9d
rocketpy <dimitr.ushakov@gmail.com>
2023-07-05 23:58:59 +0300

    Update Phonenumbers_lib.py

--- Phonenumbers_lib.py
+++ Phonenumbers_lib.py
@@ -10,3 +10,18 @@ import phonenumbers
 from phonenumbers import geocoder, carrier
 
 ph = phonenumbers.parse('+.......')
+
+
+ import phonenumbers
+>>> x = phonenumbers.parse("+442083661177", None)
+>>> print(x)
+Country Code: 44 National Number: 2083661177 Leading Zero: False
+>>> type(x)
+<class 'phonenumbers.phonenumber.PhoneNumber'>
+>>> y = phonenumbers.parse("020 8366 1177", "GB")
+>>> print(y)
+Country Code: 44 National Number: 2083661177 Leading Zero: False
+>>> x == y
+True
+>>> z = phonenumbers.parse("00 1 650 253 2222", "GB")  # as dialled from GB, not a GB number
+>>> print(z)
