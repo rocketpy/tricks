fb982de87a2a39d861abb1c4797575efe6677caa
rocketpy <dimitr.ushakov@gmail.com>
2023-07-19 23:38:26 +0300

    Update Phone_Gen.py

--- Phone_Gen.py
+++ Phone_Gen.py
@@ -3,3 +3,16 @@
 # https://pypi.org/project/phone-gen/
 
 # pip install phone-gen
+
+
+from phone_gen import PhoneNumber
+
+phone_number = PhoneNumber("GB")  # ISO 3166-2
+# or
+phone_number = PhoneNumber("GBR")  # ISO 3166-3
+# or
+phone_number = PhoneNumber("Great Britain")  # Country name
+
+# Get a phone number
+number = phone_number.get_number()
+# +442908124840
