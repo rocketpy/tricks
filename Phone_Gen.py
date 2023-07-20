7c210b4bd1088b284b92e68a507c2404f7d18be6
rocketpy <dimitr.ushakov@gmail.com>
2023-07-20 23:55:00 +0300

    Update Phone_Gen.py

--- Phone_Gen.py
+++ Phone_Gen.py
@@ -16,3 +16,19 @@ phone_number = PhoneNumber("Great Britain")  # Country name
 # Get a phone number
 number = phone_number.get_number()
 # +442908124840
+
+# Get a country code
+country_code = phone_number.get_code()
+# 44
+
+# Get a phone number without a country code
+number = phone_number.get_number(full=False)
+# 183782623
+
+# Get a mobile phone number
+number = phone_number.get_mobile()
+# +447911899521
+
+# Get a national phone number
+number = phone_number.get_national()
+# +442408055065
