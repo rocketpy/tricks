21812eb20c1c69a904aaffc918dab7cc41b44abb
rocketpy <dimitr.ushakov@gmail.com>
2023-09-06 23:43:35 +0300

    Update OpenAI's_Code_Interprete.py

--- OpenAI's_Code_Interprete.py
+++ OpenAI's_Code_Interprete.py
@@ -1,1 +1,8 @@
+# Open Interpreter - Let language models run code on your computer.
+# An open-source, locally running implementation of OpenAI's Code Interpreter.
 
+# https://github.com/KillianLucas/open-interpreter
+
+
+# pip install open-interpreter
+# interpreter
