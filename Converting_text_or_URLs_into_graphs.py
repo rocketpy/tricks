237708644596a682f650cf74031dafe5118bd082
rocketpy <dimitr.ushakov@gmail.com>
2023-09-16 23:33:14 +0300

    Update Converting_text_or_URLs_into_graphs.py

--- Converting_text_or_URLs_into_graphs.py
+++ Converting_text_or_URLs_into_graphs.py
@@ -3,3 +3,25 @@
 # https://github.com/yoheinakajima/instagraph
 
 # For non-coders: https://instagraph.ai/
+
+# Installation
+"""
+To get started, you'll need Python and pip installed.
+1. Clone the repository
+git clone https://github.com/yoheinakajima/instagraph.git
+
+2. Navigate to the project directory
+cd instagraph
+
+3. Install the required Python packages
+pip install -r requirements.txt
+
+4. Set up your OpenAI API Key
+Change .env.example to .env ''' bash mv .env.example .env ''' Add your OpenAI API key to .env file:
+OPENAI_API_KEY=your-api-key-here
+
+5. Run the Flask app
+python main.py
+Navigate to http://localhost:8080 to see your app running.
+"""
+
