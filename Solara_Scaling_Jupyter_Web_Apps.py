282c6a4aa712183e02280acf392937f6f62e8605
rocketpy <dimitr.ushakov@gmail.com>
2023-10-12 23:39:02 +0300

    Update Solara_Scaling_Jupyter_Web_Apps.py

--- Solara_Scaling_Jupyter_Web_Apps.py
+++ Solara_Scaling_Jupyter_Web_Apps.py
@@ -1,4 +1,15 @@
 # Solara - Pure Python, React-style Framework for Scaling Your Jupyter and Web Apps
 
 # https://github.com/widgetti/solara
+# https://solara.dev/docs/installing
+# pip install solara
 
+# First script
+# Put the following Python snippet in a file (we suggest sol.py), or put it in a Jupyter notebook cell:
+
+import solara
+
+# Declare reactive variables at the top level. Components using these variables
+# will be re-executed when their values change.
+sentence = solara.reactive("Solara makes our team more productive.")
+word_limit = solara.reactive(10)
