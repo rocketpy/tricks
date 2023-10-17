663f1fb82670e1c6028fe8fe8f64e890117e17a0
rocketpy <dimitr.ushakov@gmail.com>
2023-10-17 23:51:42 +0300

    Update JSON_viewer.py

--- JSON_viewer.py
+++ JSON_viewer.py
@@ -1,4 +1,19 @@
 #  Very fast JSON to view
-
 # http://jsonviewer.stack.hu/
 
+
+# parse a JSON
+import json
+
+json_string = '{...}'
+json_dict = json.loads(json_string)
+print(json_dict)
+
+# parse and read a JSON file
+with open('fcc.json', 'r') as fcc_file:
+    fcc_data = json.load(fcc_file)
+    print(fcc_data)
+
+# Pretty Print JSON data
+print(json.dumps(fcc_data, indent=4))
+print(json.dumps(fcc_data, indent=4, sort_keys=True))
