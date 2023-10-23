64e972cb913fb556eefb83a70042157e8de4a0be
rocketpy <dimitr.ushakov@gmail.com>
2023-10-23 23:49:33 +0300

    Update Encoding_Decoding_Base64.py

--- Encoding_Decoding_Base64.py
+++ Encoding_Decoding_Base64.py
@@ -1,1 +1,8 @@
+# to encode string
+import base64
 
+some_string = "Each Base64 character represents 6 bits of data"
+some_string_bytes = some_string.encode("ascii")
+base64_bytes = base64.b64encode(some_string_bytes)
+base64_string = base64_bytes.decode("ascii")
+print(f"Result: {base64_string}")
