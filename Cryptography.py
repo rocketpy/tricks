44b6aa2c1685a252bb396b9ad662fdfc78753ee5
rocketpy <dimitr.ushakov@gmail.com>
2023-10-27 23:23:45 +0300

    Update Cryptography.py

--- Cryptography.py
+++ Cryptography.py
@@ -2,3 +2,13 @@
 
 # https://pypi.org/project/cryptography/
 
+
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+f = Fernet(key)
+token = f.encrypt(b"A really secret message. Not for prying eyes.")
+print(token)
+# b'...'
+# f.decrypt(token)
+# b'A really secret message. Not for prying eyes.'
