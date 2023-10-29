cc99ee57593b9d423c21eacf4e84b37d10c7b0a7
rocketpy <dimitr.ushakov@gmail.com>
2023-10-29 23:37:13 +0200

    Update Public_APIs.py

--- Public_APIs.py
+++ Public_APIs.py
@@ -1,3 +1,13 @@
 # Public APIs - A collective list of free APIs for use in software and web development
 
 # https://github.com/public-apis/public-apis
+# API - https://api.publicapis.org/
+
+"""
+Requirements:
+certifi==2021.10.8
+charset-normalizer==2.0.10
+idna==3.3
+requests==2.27.1
+urllib3==1.26.8
+"""
