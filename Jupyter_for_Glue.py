e293940febdd1785ba9d1d83c1137187673666a6
rocketpy <dimitr.ushakov@gmail.com>
2023-11-06 23:57:07 +0200

    Create Jupyter_for_Glue.py

---
+++ Jupyter_for_Glue.py
@@ -0,0 +1,13 @@
+#  glue-jupyter - Jupyter interface for Glue
+# Glue: multi-dimensional linked-data exploration
+
+# https://github.com/glue-viz/glue-jupyter
+# http://glueviz.org/
+
+# Installing
+# pip install git+https://github.com/glue-viz/glue-jupyter.git
+
+# From source:
+# git clone https://github.com/glue-viz/glue-jupyter.git
+# cd glue-jupyter
+# pip install -e .
