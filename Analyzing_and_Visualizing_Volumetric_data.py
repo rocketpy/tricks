b1652a4a7be82a08aebf7ad4a13dad20fb83984a
rocketpy <dimitr.ushakov@gmail.com>
2023-11-08 23:48:04 +0200

    Update Analyzing_and_Visualizing_Volumetric_data.py

--- Analyzing_and_Visualizing_Volumetric_data.py
+++ Analyzing_and_Visualizing_Volumetric_data.py
@@ -2,13 +2,11 @@
 
 # https://github.com/yt-project/yt
 # https://yt-project.org/data/
+# https://github.com/yt-project/yt/tree/main/doc/source/quickstart
 
 """
 Using the Automatic Downloader
-
-For the purposes of this tutorial, or whenever you want to use sample data,
-you can use the load_sample command to utilize the pooch auto-downloader.
-For instance:
+To use the load_sample command to utilize the pooch auto-downloader.
 ds = yt.load_sample("IsolatedGalaxy")
 """
 
