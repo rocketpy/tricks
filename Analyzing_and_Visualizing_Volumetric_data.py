0a97f788f3f3a697034dc441ccc7ee2a20d210c9
rocketpy <dimitr.ushakov@gmail.com>
2023-11-08 23:46:38 +0200

    Update Analyzing_and_Visualizing_Volumetric_data.py

--- Analyzing_and_Visualizing_Volumetric_data.py
+++ Analyzing_and_Visualizing_Volumetric_data.py
@@ -1,3 +1,23 @@
 # YT - is an open-source, permissively-licensed Python library for analyzing and visualizing volumetric data.
 
 # https://github.com/yt-project/yt
+# https://yt-project.org/data/
+
+"""
+Using the Automatic Downloader
+
+For the purposes of this tutorial, or whenever you want to use sample data,
+you can use the load_sample command to utilize the pooch auto-downloader.
+For instance:
+ds = yt.load_sample("IsolatedGalaxy")
+"""
+
+
+# Simple Visualizations of Data
+import yt
+
+ds = yt.load_sample("enzo_tiny_cosmology")
+print("Redshift =", ds.current_redshift)
+
+p = yt.ProjectionPlot(ds, "y", ("gas", "density"))
+p.show()
