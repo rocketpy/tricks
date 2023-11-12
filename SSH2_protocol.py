cc486b0f0a0a0015c2fb0eaded65c4844221e506
rocketpy <dimitr.ushakov@gmail.com>
2023-11-12 23:38:32 +0200

    Update SSH2_protocol.py

--- SSH2_protocol.py
+++ SSH2_protocol.py
@@ -1,4 +1,20 @@
 # Paramiko is a pure-Python (3.6+) implementation of the SSHv2 protocol
 
 # https://pypi.org/project/paramiko/
+# https://docs.paramiko.org/en/latest/
+# https://www.paramiko.org/installing.html
+
 # pip install paramiko
+
+# For example:
+
+# Connect
+client = SSHClient()
+client.connect(host, port, username)
+# Obtain session
+session = client.get_transport().open_session()
+# Forward local agent
+AgentRequestHandler(session)
+# Commands executed after this point will see the forwarded agent on
+# the remote end.
+session.exec_command("git clone https://my.git.repository/")
