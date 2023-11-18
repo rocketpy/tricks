226a733b1356497a4a344d5b9bce5473d44519dc
rocketpy <dimitr.ushakov@gmail.com>
2023-11-18 23:49:42 +0200

    Create Multi_Agent_Postgres_Data_Analytics_Tool.py

---
+++ Multi_Agent_Postgres_Data_Analytics_Tool.py
@@ -0,0 +1,16 @@
+# Multi-Agent Postgres Data Analytics Tool
+
+# https://github.com/disler/multi-agent-postgres-data-analytics
+
+"""
+Setup:
+Read the codebase first. Remember, this is an experiment and learning tool. It's not meant to be a framework or library.
+Run git branch -a to view all branches. Each branch is a video in the series.
+git checkout <branch-name> you want to view.
+poetry install
+cp .env.sample .env
+Fill out .env with your postgres url and openai api key
+Run a prompt against your database
+poetry run start --prompt "<ask your agent a question about your postgres database>"
+Start with something simple to get a feel for it and then build up to more complex questions.
+"""
