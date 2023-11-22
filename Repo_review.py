af6a689d82d9f99489ba6bc6f7b8ff5912dad88d
rocketpy <dimitr.ushakov@gmail.com>
2023-11-22 23:25:23 +0200

    Create Repo_review.py

---
+++ Repo_review.py
@@ -0,0 +1,7 @@
+# repo-review - This is a framework for building checks designed to check to see if a repository follows guidelines. 
+
+# https://github.com/scientific-python/repo-review
+# pypi - https://pypi.org/project/repo-review/
+
+# pip install repo-review
+
