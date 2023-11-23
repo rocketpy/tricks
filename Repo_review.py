65511baad09994d5375d477f4871e7d0b7aaf611
rocketpy <dimitr.ushakov@gmail.com>
2023-11-23 23:58:31 +0200

    Update Repo_review.py

--- Repo_review.py
+++ Repo_review.py
@@ -5,3 +5,31 @@
 
 # pip install repo-review
 
+# https://pypi.org/project/sp-repo-review/
+# pip install sp-repo-review
+
+"""
+Repo-review has a CLI interface.
+
+                                                                                
+ Usage: python -m repo_review [OPTIONS] PACKAGES...                             
+                                                                                
+ Pass in a local Path or gh:org/repo@branch.                                    
+                                                                                
+╭─ Options ────────────────────────────────────────────────────────────────────╮
+│ --version                                Show the version and exit.          │
+│ --format           [rich|json|html|svg]  Select output format.               │
+│ --stderr           [rich|json|html|svg]  Select additional output format for │
+│                                          stderr. Will disable terminal       │
+│                                          escape codes for stdout for easy    │
+│                                          redirection.                        │
+│ --select           TEXT                  Only run certain checks, comma      │
+│                                          separated. All checks run if empty. │
+│ --ignore           TEXT                  Ignore a check or checks, comma     │
+│                                          separated.                          │
+│ --package-dir  -p  TEXT                  Path to python package.             │
+│ --list-all                               List all checks and exit            │
+│ --show             [all|err|errskip]     Show all (default), or just errors, │
+│                                          or errors and skips                 │
+│ --help         -h                        Show this message and exit.     
+"""
