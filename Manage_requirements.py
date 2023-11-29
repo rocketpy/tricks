9111a5a57a22f290cdc8cf7dbb5cd9bfc116c9ce
rocketpy <dimitr.ushakov@gmail.com>
2023-11-29 23:54:27 +0200

    Update Manage_requirements.py

--- Manage_requirements.py
+++ Manage_requirements.py
@@ -1,1 +1,24 @@
+# A fantastic tool to generate requirements for your Python project, and more than that.
 
+# https://github.com/damnever/pigar
+# https://pypi.org/project/pigar/
+# pip install pigar
+
+"""
+Installation
+
+pigar can run on Python 3.7+.
+
+To install it with pip, use:
+
+[sudo] pip install pigar
+
+To install it with conda, use:
+
+conda install -c conda-forge pigar
+
+To get the newest code from GitHub:
+
+pip install git+https://github.com/damnever/pigar.git@[main or other branch] --upgrade
+
+"""
