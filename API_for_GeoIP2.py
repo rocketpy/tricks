dc2d3fb43d382af123cb5dd490e8dd786932fafc
rocketpy <dimitr.ushakov@gmail.com>
2023-12-03 23:32:22 +0200

    Update API_for_GeoIP2.py

--- API_for_GeoIP2.py
+++ API_for_GeoIP2.py
@@ -29,3 +29,26 @@ with geoip2.webservice.Client(42, 'license_key') as client:
     response.location.latitude
     response.location.longitude
     response.traits.network
+
+
+# Async Web Service Example
+import asyncio
+import geoip2.webservice
+
+
+async def main():
+    # This creates an AsyncClient object that can be reused across
+    # requests on the running event loop. If you are using multiple event
+    # loops, you must ensure the object is not used on another loop.
+    #
+    # Replace "42" with your account ID and "license_key" with your license
+    # key. Set the "host" keyword argument to "geolite.info" to use the
+    # GeoLite2 web service instead of the GeoIP2 web service. Set the
+    # "host" keyword argument to "sandbox.maxmind.com" to use the Sandbox
+    # GeoIP2 web service instead of the production GeoIP2 web service.
+    async with geoip2.webservice.AsyncClient(42, 'license_key') as client:
+    
+        # Replace "city" with the method corresponding to the web service
+        # that you are using, i.e., "country", "city", or "insights". Please
+        # note that Insights is not supported by the GeoLite2 web service.
+        response = await client.city('203.0.113.0')
