e3aee14f3934e6cd71e81bf01e8cb5d23452853c
rocketpy <dimitr.ushakov@gmail.com>
2023-12-24 23:58:39 +0200

    Create AppAgent.py

---
+++ AppAgent.py
@@ -0,0 +1,6 @@
+# AppAgent - LLM-based multimodal agent framework designed to operate smartphone applications.
+
+# https://github.com/mnotgod96/AppAgent
+
+# cd AppAgent
+# pip install -r requirements.txt
