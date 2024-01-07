886367326f7f730b44bb44461eab6978b1186fff
rocketpy <dimitr.ushakov@gmail.com>
2024-01-07 21:52:38 +0200

    Create SQL_IDE.py

---
+++ SQL_IDE.py
@@ -0,0 +1,10 @@
+# Harlequin - SQL IDE
+
+# https://github.com/tconbeer/harlequin
+
+"""
+Installing Harlequin
+After installing Python 3.8 or above, install Harlequin using pip or pipx with:
+
+pipx install harlequin
+"""
