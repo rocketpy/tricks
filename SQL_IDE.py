e3176031cd5057de431cf2a2257bf063df5b64c8
rocketpy <dimitr.ushakov@gmail.com>
2024-01-08 23:50:38 +0200

    Update SQL_IDE.py

--- SQL_IDE.py
+++ SQL_IDE.py
@@ -7,4 +7,15 @@ Installing Harlequin
 After installing Python 3.8 or above, install Harlequin using pip or pipx with:
 
 pipx install harlequin
+
+
+Using Harlequin with DuckDB
+
+From any shell, to open one or more DuckDB database files:
+
+harlequin "path/to/duck.db" "another_duck.db"
+
+To open an in-memory DuckDB session, run Harlequin with no arguments:
+
+harlequin
 """
