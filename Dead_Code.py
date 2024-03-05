32a2e713595619b3f9ec67eb5232154eac03d24d
rocketpy <dimitr.ushakov@gmail.com>
2024-03-05 23:44:37 +0200

    Update Dead_Code.py

--- Dead_Code.py
+++ Dead_Code.py
@@ -12,3 +12,12 @@
 # Or with command line options:
 
 # deadcode . --exclude=venv,tests --ignore-names=BaseTestCase,*Mixin --ignore-names-in-files=migrations
+
+
+# The same options can be provided in pyproject.toml settings file:
+"""
+[tool.deadcode]
+exclude = ["venv", "tests"]
+ignore-names = ["BaseTestCase", "*Mixin"]
+ignore-names-in-files = ["migrations"]
+"""
