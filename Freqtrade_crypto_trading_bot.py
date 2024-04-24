cc1a89e3b85a004295ac79a3731abe244f9d2b91
rocketpy <dimitr.ushakov@gmail.com>
2024-04-24 23:55:53 +0300

    Update Freqtrade_crypto_trading_bot.py

--- Freqtrade_crypto_trading_bot.py
+++ Freqtrade_crypto_trading_bot.py
@@ -2,3 +2,50 @@
 
 # https://github.com/freqtrade/freqtrade
 # https://www.freqtrade.io/en/stable/
+
+
+# Bot commands
+"""
+usage: freqtrade [-h] [-V]
+                 {trade,create-userdir,new-config,new-strategy,download-data,convert-data,convert-trade-data,list-data,backtesting,edge,hyperopt,
+                 hyperopt-list,hyperopt-show,list-exchanges,list-hyperopts,list-markets,list-pairs,list-strategies,list-timeframes,show-trades,
+                 test-pairlist,install-ui,plot-dataframe,plot-profit,webserver}
+                 ...
+
+Free, open source crypto trading bot
+
+positional arguments:
+  {trade,create-userdir,new-config,new-strategy,download-data,convert-data,convert-trade-data,list-data,backtesting,edge,hyperopt,
+  hyperopt-list,hyperopt-show,list-exchanges,list-hyperopts,list-markets,list-pairs,list-strategies,list-timeframes,show-trades,
+  test-pairlist,install-ui,plot-dataframe,plot-profit,webserver}
+    trade               Trade module.
+    create-userdir      Create user-data directory.
+    new-config          Create new config
+    new-strategy        Create new strategy
+    download-data       Download backtesting data.
+    convert-data        Convert candle (OHLCV) data from one format to
+                        another.
+    convert-trade-data  Convert trade data from one format to another.
+    list-data           List downloaded data.
+    backtesting         Backtesting module.
+    edge                Edge module.
+    hyperopt            Hyperopt module.
+    hyperopt-list       List Hyperopt results
+    hyperopt-show       Show details of Hyperopt results
+    list-exchanges      Print available exchanges.
+    list-hyperopts      Print available hyperopt classes.
+    list-markets        Print markets on exchange.
+    list-pairs          Print pairs on exchange.
+    list-strategies     Print available strategies.
+    list-timeframes     Print available timeframes for the exchange.
+    show-trades         Show trades.
+    test-pairlist       Test your pairlist configuration.
+    install-ui          Install FreqUI
+    plot-dataframe      Plot candles with indicators.
+    plot-profit         Generate plot showing profits.
+    webserver           Webserver module.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -V, --version         show program's version number and exit
+  """
