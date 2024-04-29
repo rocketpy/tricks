58411fe65947231f0cebceee0689744022d931d9
rocketpy <dimitr.ushakov@gmail.com>
2024-04-29 23:49:36 +0300

    Update BBot_2.py

--- BBot_2.py
+++ BBot_2.py
@@ -23,3 +23,58 @@ trigger = ["Hello","Hi there","Hey","Greetings","Good morning","Good afternoon",
 closing = ["Goodbye","Farewell","See you later","Take care","Adios","Sayonara","Cheerio","Till we meet again","Catch you later","So long",
         "Bye bye","Auf Wiedersehen","Arrivederci","Hasta luego","Until next time","Thanks","Thank you","Thanks a lot","Thank you very much","Appreciate it","Much obliged",
         "Gracias","Arigato","Merci","Domo arigato","Cheers","See you soon","Take it easy"]
+
+def vision():
+    print("1")
+    while True:
+        # print("Loop 1 is running")
+        watch()
+        time.sleep(0.08)  # Simulate some work
+        pause_event.wait()
+
+def talk():
+    print("2")
+    while True:
+        print("Loop 2 is running")
+        que = listen()
+
+        # print(ask(str(que)))
+
+        # --- conditions ---
+
+        if any(str(item) in que for item in trigger):
+            print("3")
+            # pause_event.clear()   # pause other threads 
+            reply=ask(str(que))
+            # pause_event.set()     # continue other threads 
+
+            print(reply)
+            # speak(reply)
+
+            # continue listening ---
+            while True:
+                print("4")
+                que = listen()
+
+                if any(str(item) in que for item in closing):
+                    print("5")
+                    # pause_event.clear()
+                    reply= ask(str(que))
+                    # pause_event.set()
+                    
+                    print(reply)
+                    # speak(reply)
+                    break
+
+                # pause_event.clear()
+                ans = ask(str(que))
+                # pause_event.set()
+
+                print(ans)
+
+
+    # speak(str(ans))
+        time.sleep(0.01)  # Simulate some work
+
+# camera - face detect - position 
+#  listen - txt - check -  ask - speak
