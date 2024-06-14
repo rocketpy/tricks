83621ef11b7e05b5e3875d21c6ef7e6f079fe567
rocketpy <dimitr.ushakov@gmail.com>
2024-06-14 23:58:32 +0300

    Create Translation_Agent.py

---
+++ Translation_Agent.py
@@ -0,0 +1,14 @@
+# Translation Agent - Agentic translation using reflection workflow
+
+# https://github.com/andrewyng/translation-agent
+
+
+# Features:
+"""
+This is a Python demonstration of a reflection agentic workflow for machine translation. The main steps are:
+
+Prompt an LLM to translate a text from source_language to target_language.
+Have the LLM reflect on the translation to come up with constructive suggestions for improving it.
+Use the suggestions to improve the translation.
+"""
+
