46ff59222d2ecbc05130f2a1ce46e2b607731e0b
rocketpy <dimitr.ushakov@gmail.com>
2024-06-15 23:55:23 +0300

    Update Translation_Agent.py

--- Translation_Agent.py
+++ Translation_Agent.py
@@ -3,12 +3,26 @@
 # https://github.com/andrewyng/translation-agent
 
 
-# Features:
 """
+# Features:
 This is a Python demonstration of a reflection agentic workflow for machine translation. The main steps are:
 
 Prompt an LLM to translate a text from source_language to target_language.
 Have the LLM reflect on the translation to come up with constructive suggestions for improving it.
 Use the suggestions to improve the translation.
-"""
 
+
+# Examples
+This directory contains example scripts demonstrating the usage of the translation-agent workflow.
+
+# Contents
+example_script.py: A simple script showing how to perform machine translation using the package.
+sample-texts/: A directory containing a few sample texts from The Batch letters written by Andrew and Data Points summaries found on the DeepLearning.ai website.
+
+# Usage
+To run the example scripts, ensure that you have installed the translation-agent package and have activated your virtual environment.
+
+# Then run:
+
+python example_script.py
+"""
