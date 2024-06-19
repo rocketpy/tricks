41ac230c876f488e8f9c956c005b21eb3ec09fb6
rocketpy <dimitr.ushakov@gmail.com>
2024-06-19 23:37:57 +0300

    Create Python_packaging_build_frontend.py

---
+++ Python_packaging_build_frontend.py
@@ -0,0 +1,15 @@
+# build - A simple, correct Python build frontend.
+
+# https://github.com/pypa/build
+# https://build.pypa.io/en/stable/
+
+
+"""
+Installation - build can be installed via pip or an equivalent via:
+
+$ pip install build
+
+Usage
+
+$ python -m build
+"""
