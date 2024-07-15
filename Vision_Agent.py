d14a9f4d1fbd269daa1c72c820416c43e2fc5424
rocketpy <dimitr.ushakov@gmail.com>
2024-07-15 23:46:01 +0300

    Create Vision_Agent.py

---
+++ Vision_Agent.py
@@ -0,0 +1,6 @@
+# Vision Agent - is a library that helps you utilize agent frameworks to generate code to solve your vision task.
+
+# https://github.com/landing-ai/vision-agent
+
+
+# pip install vision-agent
