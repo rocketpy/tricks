be63b3cca34205cdfca81366afaf1ff6aa616e74
rocketpy <dimitr.ushakov@gmail.com>
2024-09-03 23:51:12 +0300

    Create Hivision_ID_Photos.py

---
+++ Hivision_ID_Photos.py
@@ -0,0 +1,6 @@
+# HivisionIDPhotos - aims to develop a practical intelligent algorithm for producing ID photos. 
+# It uses a complete set of model workflows to recognize various user photo scenarios, perform image segmentation, and generate ID photos.
+
+# https://github.com/Zeyi-Lin/HivisionIDPhotos
+# https://github.com/Zeyi-Lin/HivisionIDPhotos/blob/master/README_EN.md
+
