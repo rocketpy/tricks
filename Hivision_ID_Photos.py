a86039d7b40511dce13b86b246e7cf65582e2983
rocketpy <dimitr.ushakov@gmail.com>
2024-09-05 23:53:21 +0300

    Update Hivision_ID_Photos.py

--- Hivision_ID_Photos.py
+++ Hivision_ID_Photos.py
@@ -4,3 +4,30 @@
 # https://github.com/Zeyi-Lin/HivisionIDPhotos
 # https://github.com/Zeyi-Lin/HivisionIDPhotos/blob/master/README_EN.md
 
+# Features:
+"""
+Perform lightweight image segmentation (Only CPU is needed for fast inference.)
+Generate standard ID photos and six-inch layout photos according to different size specifications
+Provide beauty features (waiting)
+Provide intelligent formal wear replacement (waiting)
+"""
+
+# Environment Dependencies and Installation
+"""
+Python >= 3.7 (The main test of the project is in Python 3.10.)
+onnxruntime
+OpenCV
+Option: Linux, Windows, MacOS
+
+# Installation
+
+Clone repo
+git clone https://github.com/Zeyi-Lin/HivisionIDPhotos.git
+cd  HivisionIDPhotos
+
+(Important) Install dependent packages
+It is recommended to create a Python 3.10 virtual environment with conda and then execute the following command.
+
+pip install -r requirements.txt
+pip install -r requirements-app.txt
+"""
