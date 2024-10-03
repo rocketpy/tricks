96a7f1ed91d03eb5c47f146382eed6d0508841cd
rocketpy <dimitr.ushakov@gmail.com>
2024-10-03 23:28:01 +0300

    Update Composio_integrate_AI_Agent.py

--- Composio_integrate_AI_Agent.py
+++ Composio_integrate_AI_Agent.py
@@ -22,3 +22,24 @@ composio_tool_set = ComposioToolSet()
 actions = composio_tool_set.get_actions(
     actions=[Action.GITHUB_STAR_A_REPOSITORY_FOR_THE_AUTHENTICATED_USER]
 )
+
+my_task = "Star a repo composiodev/composio on GitHub"
+
+# Setup openai assistant
+assistant_instruction = "You are a super intelligent personal assistant"
+
+assistant = openai_client.beta.assistants.create(
+    name="Personal Assistant",
+    instructions=assistant_instruction,
+    model="gpt-4-turbo",
+    tools=actions,
+)
+
+# create a thread
+thread = openai_client.beta.threads.create()
+
+message = openai_client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content=my_task
+)
