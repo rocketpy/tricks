cf6075a77917464665ea7a7de11ae9c1654e30e3
rocketpy <dimitr.ushakov@gmail.com>
2024-12-23 23:09:55 +0200

    Create Apple_FindMy.py

---
+++ Apple_FindMy.py
@@ -0,0 +1,11 @@
+# FindMy - The all-in-one library that provides everything you need to query Apple's FindMy network!
+
+# https://github.com/malmeloo/FindMy.py
+
+
+# Important !!!
+
+# This project is currently in Alpha. While existing functionality will likely not change much, the API design is subject to change without prior warning.
+
+# You are encouraged to report any issues you can find on the issue tracker - https://github.com/malmeloo/FindMy.py/issues/
+
