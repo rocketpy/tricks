250c54b1dd9a3a677ae7d32a5c10cc2156667b19
rocketpy <dimitr.ushakov@gmail.com>
2024-12-26 22:18:47 +0200

    Update Apple_FindMy.py

--- Apple_FindMy.py
+++ Apple_FindMy.py
@@ -6,6 +6,20 @@
 # Important !!!
 
 # This project is currently in Alpha. While existing functionality will likely not change much, the API design is subject to change without prior warning.
-
 # You are encouraged to report any issues you can find on the issue tracker - https://github.com/malmeloo/FindMy.py/issues/
 
+
+# Features:
+"""
+ Cross-platform: no Mac needed
+ Fetch and decrypt location reports
+ Official accessories (AirTags, iDevices, etc.)
+ Custom AirTags (OpenHaystack)
+ Apple account sign-in
+ SMS 2FA support
+ Trusted Device 2FA support
+ Scan for nearby FindMy-devices
+ Decode their info, such as public keys and status bytes
+ Import or create your own accessory keys
+ Both async and sync APIs
+"""
