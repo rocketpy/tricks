c383d5220bf97d67ac656e74548ebbb97a6d9029
rocketpy <dimitr.ushakov@gmail.com>
2025-04-07 23:22:03 +0300

    Create Any_Docs_To_MarkDown.py

---
+++ Any_Docs_To_MarkDown.py
@@ -0,0 +1,5 @@
+# MarkItDown - MarkItDown now offers an MCP (Model Context Protocol) server for integration with LLM applications like Claude Desktop.
+
+
+# https://github.com/microsoft/markitdown
+
